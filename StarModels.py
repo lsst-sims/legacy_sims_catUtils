5639a7abb46140e3080d479c03e85065023cf60c
Simon Krughoff <krughoff@uw.edu>
2014-03-16 12:33:50 -0700

    Implemented stub for RRLy stars

--- StarModels.py
+++ StarModels.py
@@ -48,3 +48,28 @@ class WdStarObj(DBObject):
                ('radialVelocity', 'vrad'),
                ('variabilityParameters', 'varParamStr', str, 256),
                ('sedFilename', 'sedfilename', unicode, 40)]
+
+class WdStarObj(DBObject):
+    objid = 'rrly'
+    tableid = 'starsRRly'
+    idColKey = 'id'
+    raColName = 'ra'
+    decColName = 'decl'
+    objectTypeId = 5
+    spatialModel = 'POINT'
+    dbDefaultValues = {'varsimobjid':-1, 'runid':-1, 'ismultiple':-1, 'run':-1,
+                       'runobjid':-1}
+    #These types should be matched to the database.
+    #: Default map is float.  If the column mapping is the same as the column name, None can be specified
+    columns = [('id','simobjid', int),
+               ('raJ2000', 'ra*PI()/180.'),
+               ('decJ2000', 'decl*PI()/180.'),
+               ('glon', 'gal_l*PI()/180.'),
+               ('glat', 'gal_b*PI()/180.'),
+               ('magNorm', '(-2.5*log(flux_scale)/log(10.)) - 18.402732642'),
+               ('properMotionRa', '(mudecl/(1000.*3600.))*PI()/180.'),
+               ('properMotionDec', '(mura/(1000.*3600.))*PI()/180.'),
+               ('galacticAv', 'CONVERT(float, ebv*3.1)'),
+               ('radialVelocity', 'vrad'),
+               ('variabilityParameters', 'varParamStr', str, 256),
+               ('sedFilename', 'sedfilename', unicode, 40)]
