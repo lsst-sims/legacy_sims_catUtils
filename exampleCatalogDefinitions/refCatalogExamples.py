ef095da8b19d555108903e97cb26bcf3fad00c2b
danielsf <scott.f.daniel@gmail.com>
2014-03-31 10:17:51 -0700

    updated refCatalogExamples.py to inherit from PhotometryGalaxies

--- exampleCatalogDefinitions/refCatalogExamples.py
+++ exampleCatalogDefinitions/refCatalogExamples.py
@@ -2,61 +2,10 @@
 import numpy
 from lsst.sims.catalogs.measures.instance import InstanceCatalog, compound
 from lsst.sims.catalogs.measures.example_utils import ValidationUtils
+from lsst.sims.catalogs.measures.astrometry.Astrometry import Astrometry
+from lsst.sims.catalogs.measures.photometry.Photometry import PhotometryGalaxies, PhotometryStars
 
-class PhotometryMixin(object):
-    
-
-    def get_ug_color(self):
-        u = self.column_by_name('umag')
-        g = self.column_by_name('gmag')
-        return u - g
-
-    def get_gr_color(self):
-        g = self.column_by_name('gmag')
-        r = self.column_by_name('rmag')
-        return g - r
-
-    @compound('uRecalc', 'gRecalc', 'rRecalc', 'iRecalc', 'zRecalc', 'yRecalc',
-              'uBulge', 'gBulge', 'rBulge', 'iBulge', 'zBulge', 'yBulge',
-              'uDisk', 'gDisk', 'rDisk', 'iDisk', 'zDisk', 'yDisk',
-              'uAgn', 'gAgn', 'rAgn', 'iAgn', 'zAgn', 'yAgn')
-    def get_allMags(self):
-        retMags, retbMags, retdMags, retaMags = ValidationUtils.calcTotalLSSTMags(self, bandpasses=['u','g','r','i','z','y'])
-        return (numpy.array(retMags['u'], dtype=float), 
-                numpy.array(retMags['g'], dtype=float),
-                numpy.array(retMags['r'], dtype=float),
-                numpy.array(retMags['i'], dtype=float),
-                numpy.array(retMags['z'], dtype=float),
-                numpy.array(retMags['y'], dtype=float),
-                numpy.array(retbMags['u'], dtype=float), 
-                numpy.array(retbMags['g'], dtype=float),
-                numpy.array(retbMags['r'], dtype=float),
-                numpy.array(retbMags['i'], dtype=float),
-                numpy.array(retbMags['z'], dtype=float),
-                numpy.array(retbMags['y'], dtype=float),
-                numpy.array(retdMags['u'], dtype=float), 
-                numpy.array(retdMags['g'], dtype=float),
-                numpy.array(retdMags['r'], dtype=float),
-                numpy.array(retdMags['i'], dtype=float),
-                numpy.array(retdMags['z'], dtype=float),
-                numpy.array(retdMags['y'], dtype=float),
-                numpy.array(retaMags['u'], dtype=float), 
-                numpy.array(retaMags['g'], dtype=float),
-                numpy.array(retaMags['r'], dtype=float),
-                numpy.array(retaMags['i'], dtype=float),
-                numpy.array(retaMags['z'], dtype=float),
-                numpy.array(retaMags['y'], dtype=float))
-
-
-
-class AstrometryMixin(object):
-    @compound('ra_corr', 'dec_corr')
-    def get_points(self):
-        return (self.column_by_name('raJ2000') + 0.001,
-                self.column_by_name('decJ2000') - 0.001)
-
-
-class RefCatalogGalaxyBase(InstanceCatalog, AstrometryMixin, PhotometryMixin):
+class RefCatalogGalaxyBase(InstanceCatalog, Astrometry, PhotometryGalaxies):
     comment_char = ''
     catalog_type = 'ref_catalog_galaxy'
     column_outputs = ['uniqueId', 'objId', 'galid', 'meanRaJ2000', 'meanDecJ2000', 'redshift',
