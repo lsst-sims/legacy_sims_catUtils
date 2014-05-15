b3800bfa1e14ad562440a8d01ef35f7083f41f12
Simon Krughoff <simon.krughoff@gmail.com>
2014-05-14 17:40:24 -0700

    Base catalog models

--- OpSim3_61DBObject.py
+++ OpSim3_61DBObject.py
@@ -1,43 +1,9 @@
-from dbConnection import ChunkIterator, DBObject
+from lsst.sims.catalogs.generation.db import ChunkIterator, DBObject, ObservationMetaData
 from collections import OrderedDict
 import numpy
 import math
 
-class ObservationMetaData(object):
-    """Observation Metadata
-    
-    This class contains any metadata for a query which is associated with
-    a particular telescope pointing, including bounds in RA and DEC, and
-    the time of the observation.
-
-    **Parameters**
-
-        * circ_bounds : dict (optional)
-          a dictionary with the keys 'ra', 'dec', and 'radius' measured, in
-          degrees
-        * box_bounds : dict (optional)
-          a dictionary with the keys 'ra_min', 'ra_max', 'dec_min', 'dec_max',
-          measured in degrees
-        * mjd : float (optional)
-          The MJD of the observation..
-        * metadata : dict (optional)
-          a dictionary containing arbitrary metadata
-
-    **Examples**::
-
-        >>> data = ObservationMetaData(dict(('ra_min', 0), ('ra_max', 10), ('dec_min', 10), ('dec_max', 20)))
-
-    """
-            
-    def __init__(self, circ_bounds=None, box_bounds=None, mjd=None, metadata=None):
-        if circ_bounds is not None and box_bounds is not None:
-            raise ValueError("Passing both circ_bounds and box_bounds")
-        self.circ_bounds = circ_bounds
-        self.box_bounds = box_bounds
-        self.mjd = mjd
-        self.metadata = metadata
-
-class MetaDataDBObject(DBObject):
+class OpSim3_61DBObject(DBObject):
     """Meta Data Database Object base class
 
     """
