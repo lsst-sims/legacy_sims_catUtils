555ec03851392d444f71f0fb5a628103812b8443
danielsf <scott.f.daniel@gmail.com>
2014-10-15 09:19:42 -0700

    changed a reference to DBObject.from_objid that did not get updated in makeGalaxyPhotCat.py

--- examples/makeGalaxyPhotCat.py
+++ examples/makeGalaxyPhotCat.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     obs_metadata = ObservationMetaData(boundType='circle', unrefractedRA=0.0, unrefractedDec=0.0,
                                        boundLength=0.01)
-    dbobj = DBObject.from_objid('galaxyBase')
+    dbobj = CatalogDBObject.from_objid('galaxyBase')
     t = dbobj.getCatalog('galaxy_photometry_cat', obs_metadata=obs_metadata)
     filename = 'galaxy_photometry_test.dat'
     t.write_catalog(filename, chunk_size=10000)
