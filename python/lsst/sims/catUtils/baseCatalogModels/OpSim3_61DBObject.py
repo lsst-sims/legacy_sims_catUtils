923c19ca4594bb844b02fb91c6fedc8b41748706
danielsf <scott.f.daniel@gmail.com>
2014-10-31 15:02:57 -0700

    added documentation to OpSim3_61DBObject.py
    also removed an unnecessary kwarg from the getObservationMetaData definition

--- python/lsst/sims/catUtils/baseCatalogModels/OpSim3_61DBObject.py
+++ python/lsst/sims/catUtils/baseCatalogModels/OpSim3_61DBObject.py
@@ -7,19 +7,27 @@ import math
 __all__ = ["OpSim3_61DBObject"]
 
 class OpSim3_61DBObject(DBObject):
-    """Meta Data Database Object base class
-
+    """
+    This is a class which allows you to query the databse of OpSim runs (v 3_61)
     """
     #: This is the default address.  Simply change this in the class definition for other
     #: endpoints.
     dbAddress = "mssql+pymssql://LSST-2:L$$TUser@fatboy.npl.washington.edu:1433/LSST"
 
+    #This is the table that this DBObject is querying
     tableid = 'output_opsim3_61'
 
     #: These are interpreted as SQL strings.
+    #They are passed to SpatialBounds objects
+    #and indicate the RA and Dec columns in degrees
     raColName = 'fieldra*180./PI()'
     decColName = 'fielddec*180./PI()'
 
+    #columnNames maps column names as stored in the database
+    #to column names as are required by PhoSim.
+    #For each tuple, the first string is the name
+    #required by PhoSim.  The second string is
+    #the name stored in the database
     columnNames = [('Opsim_obshistid','obshistid'),
                ('SIM_SEED','expdate'),
                ('Unrefracted_RA','fieldra'),
@@ -40,6 +48,9 @@ class OpSim3_61DBObject(DBObject):
                ('exptime','exptime'),
                ('airmass','airmass')]
 
+    #columnTypes is a dict indicating the data types
+    #of the columns referred to in columnNames.
+    #columns not mentioned are floats
     columnTypes ={'SIM_SEED':int,
                   'Opsim_filter':(str,1),
                   'Opsim_obshistid':numpy.int64}
@@ -48,6 +59,12 @@ class OpSim3_61DBObject(DBObject):
         super(OpSim3_61DBObject, self).__init__(address=address)
 
     def getBasicQuery(self):
+        """
+        This class creates a string that is an SQL query of all the
+        columns needed to create an ObservationMetaData suitable for
+        generating a PhoSim input catalog.  No constraints
+        are placed on the query (it will grab all entries in the database)
+        """
         query = 'SELECT '
         for name in self.columnNames:
             if query != 'SELECT ':
@@ -60,6 +77,20 @@ class OpSim3_61DBObject(DBObject):
         return query, dtype
 
     def executeConstrainedQuery(self, spatialBound, constraint=None):
+        """
+        This method will perform a query which is contrained on the sky by spatialBound.
+        You can add a supplementary constraint on another column using the 'contraint' kwarg.
+
+        param [in] spatialBound a SpatialBound object specifying the region of the sky from
+        which you want your pointings drawn
+
+        param [in] constraint an optional constraint on another column.  Should be a string
+        of the form 'airmass=1.1'
+
+        param [out] a recarray of all of the pointings matching your constraints.  This recarray
+        will contain all of the columns necessary for constructing an ObservationMetaData object
+        suitable for generating a PhoSim input catalog.
+        """
         query, dtype = self.getBasicQuery()
         query += ' WHERE '+spatialBound.to_SQL(self.raColName, self.decColName)
         if constraint is not None:
@@ -69,8 +100,23 @@ class OpSim3_61DBObject(DBObject):
         results = self.execute_arbitrary(query, dtype=dtype)
         return results
 
-    def getObservationMetaData(self, obshistid, radiusDeg, makeCircBounds=True, makeBoxBounds=False, colnames=None):
+    def getObservationMetaData(self, obshistid, radiusDeg, makeCircBounds=True, makeBoxBounds=False):
+        """
+        This class creates an ObservationMetaData object suitable for generating a PhoSim input
+        catalog based on an actual OpSim pointing.
 
+        param [in] obshistid is the obshistid of the pointing to be used
+
+        param [in] radiusDeg is the radius (in degrees) of the desired region on the sky.
+        If you have specified a box bounds this will be half the length of each side of the box.
+
+        param [in] makeCircBounds a boolean telling this method to construct an ObservationMetaData
+        for a circular region of the sky centered on the specified OpSim pointing
+
+        param [in] make BoxBounds a boolean telling this method to construct an
+        ObservationMetaData for a square region of the sky centered on the specified
+        OpSim pointing
+        """
         query, dtype = self.getBasicQuery()
         query += ' WHERE obshistid = %i' % obshistid
 
