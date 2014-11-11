407b161d3b8ad30a4975a35ddeb459e114f9940c
danielsf <scott.f.daniel@gmail.com>
2014-11-10 16:38:47 -0800

    cleaned up whitespace

--- examples/galsimInterpreter.py
+++ examples/galsimInterpreter.py
@@ -6,7 +6,7 @@ from lsst.sims.photUtils import Sed, Bandpass
 class GalSimDetector(object):
     def __init__(self, name=None, xCenter=None, yCenter=None,
                  xMin=None, xMax=None, yMin=None, yMax=None):
-    
+
         self.name = name
         self.xCenter = xCenter
         self.yCenter = yCenter
@@ -25,7 +25,7 @@ class GalSimInterpreter(object):
         self.sedDir = os.getenv('SIMS_SED_LIBRARY_DIR')
         self.data = None
         self.detectors = []
-        
+
     def readCatalog(self, catalogFile):
         dataNeeded = ['x_pupil', 'y_pupil', 'magNorm', 'sedFilepath',
                       'redshift', 'positionAngle',
@@ -45,19 +45,19 @@ class GalSimInterpreter(object):
         cat = open(catalogFile,'r')
         lines = cat.readlines()
         cat.close()
-        
+
         for line in lines:
             if line[0] != '#':
                 break
-       
+
             line = line.replace("#","").strip()
             line = numpy.array(line.split(';'))
-            
+
             if line[0] == 'galSimType':
                 dtype = numpy.dtype(
                         [(ww, dataTypes[ww]) if ww in dataTypes else (ww, defaultType) for ww in line]
                         )
-                        
+
             if line[0] == 'detector':
                 name = line[1]
                 xCenter = float(line[2])
@@ -66,32 +66,32 @@ class GalSimInterpreter(object):
                 xMax = float(line[5])
                 yMin = float(line[6])
                 yMax = float(line[7])
-                
+
                 detector = GalSimDetector(name=name, xCenter=xCenter, yCenter=yCenter,
                                           xMin=xMin, xMax=xMax, yMin=yMin, yMax=yMax)
                 self.detectors.append(detector)
-        
+
         print 'n_detectors ',len(self.detectors)
         self.data = numpy.genfromtxt(catalogFile, dtype=dtype, delimiter=';')
-    
+
     def drawCatalog(self, fileNameRoot=None, bandPass=None):
         for detector in self.detectors:
             self.drawImage(fileNameRoot=fileNameRoot, bandPass=bandPass, detector=detector)
-    
+
     def drawImage(self, fileNameRoot=None, bandPass=None, detector=None):
         self.bandPass = galsim.Bandpass(bandPass)
-        
+
         detectorName = detector.name
         detectorName = detectorName.replace(',','_')
         detectorName = detectorName.replace(':','_')
         detectorName = detectorName.replace(' ','_')
-        
+
         fileName = fileNameRoot+detectorName+'.fits'
-        
+
         nx = int((detector.xMax - detector.xMin)/self.scale)
         ny = int((detector.yMax - detector.yMin)/self.scale)
         image = galsim.Image(nx,ny)
-        
+
         drawn = 0
         if self.data is not None:
             for entry in self.data:
@@ -107,24 +107,24 @@ class GalSimInterpreter(object):
                         self.drawGalaxy(entry=entry, image=image, detector=detector)
                     else:
                         print entry['galSimType']
-        
+
         if drawn>0:
             image.write(fileName)
-        
+
     def drawGalaxy(self, entry=None, image=None, detector=None):
 
         sedFile = os.path.join(self.sedDir,entry['sedFilepath'])
-        
+
         obj = galsim.Sersic(n=entry['sindex'], half_light_radius=entry['halfLightRadius'],
                             gsparams=self.bigfft)
-        
+
         obj = obj.shear(q=entry['minorAxis']/entry['majorAxis'], beta=entry['positionAngle']*galsim.radians)
-    
+
         dx=entry['x_pupil']-detector.xCenter
         dy=entry['y_pupil']-detector.yCenter
-    
+
         obj = obj.shift(dx, dy)
-                         
+
         sed = Sed()
         sed.readSED_flambda(sedFile)
         fNorm = sed.calcFluxNorm(entry['magNorm'], self.imsimband)
@@ -133,16 +133,16 @@ class GalSimInterpreter(object):
         sed.addCCMDust(a_int, b_int, A_v=entry['internalAv'], R_v=entry['internalRv'])
         sed.redshiftSED(entry['redshift'], dimming=False)
         sed.addCCMDust(a_int, b_int, A_v=entry['galacticAv'], R_v=entry['galacticRv'])
-        
+
         spectrum = galsim.SED(spec = lambda ll:
                                  numpy.interp(ll, sed.wavelen, sed.flambda),
                                  flux_type='flambda')
-        
+
         obj = obj*spectrum
-        
+
         image = obj.drawImage(bandpass=self.bandPass, scale=self.scale, image=image,
                                   add_to_image=True, method='real_space')
- 
+
 
 
 bandPass = os.path.join(os.getenv('THROUGHPUTS_DIR'),'baseline','total_g.dat')
@@ -152,4 +152,4 @@ gs.readCatalog('galsim_example.txt')
 
 name = 'galsimTest_'
 gs.drawCatalog(bandPass=bandPass, fileNameRoot=name)
- 
+
