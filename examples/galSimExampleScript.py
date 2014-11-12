ccd31eee3686d4a886f013bf9378e8c016ac8cc7
danielsf <scott.f.daniel@gmail.com>
2014-11-12 15:28:12 -0800

    added comments to galSimExampleScript.py

--- examples/galSimExampleScript.py
+++ examples/galSimExampleScript.py
@@ -1,6 +1,18 @@
 """
 This file shows how to generate a GalSim input catalog named galSim_example.txt
+and then read that file in using the GalSimInterpreter class (defined in
+exampleCatalogDefinitions/galSimInterpeter.py) and turn them into FITS images.
 
+This somewhat convoluted API (writing the catalog to a file and then reading it
+in with a separate script that uses GalSim to produce an image) is made necessary
+by the fact that we cannot install GalSim using the Anaconda version of Python on
+Macs
+
+stackoverflow.com/questions/23771608/trouble-installing-galsim-on-osx-with-anaconda
+
+To wit: if the user has a system python, SCons will try to install galsim using that
+python even if the user tells SCons to use the Anaconda python.  Therefore, we cannot
+use the stack-supplied python to run GalSim directly for the user.
 """
 
 from __future__ import with_statement
@@ -14,21 +26,25 @@ from lsst.sims.catUtils.exampleCatalogDefinitions import GalSimInterpreter
 
 from lsst.sims.catUtils.baseCatalogModels import *
 
-#starObjNames = ['msstars', 'bhbstars', 'wdstars', 'rrlystars', 'cepheidstars']
-
+#select an OpSim pointing
 obsMD = CatalogDBObject.from_objid('opsim3_61')
 obs_metadata = obsMD.getObservationMetaData(88625744, 0.01, makeCircBounds = True)
 
+#grab a database of galaxies (in this case, galaxy bulges)
 gals = CatalogDBObject.from_objid('galaxyBulge')
 
 #now append a bunch of objects with 2D sersic profiles to our output file
 galaxy_galSim = GalSimGalaxies(gals, obs_metadata=obs_metadata)
 galaxy_galSim.write_catalog('galSim_example.txt')
 
+#specify a bandpass through which to observe the galaxies
 bandPass = os.path.join(os.getenv('THROUGHPUTS_DIR'),'baseline','total_g.dat')
 
 gs = GalSimInterpreter()
+
+#read in our catalog of galaxy bulges
 gs.readCatalog('galsim_example.txt')
 
+#write the images to files of the name galsimTest_detectorName.fits
 name = 'galsimTest_'
 gs.drawCatalog(bandPass=bandPass, fileNameRoot=name)
