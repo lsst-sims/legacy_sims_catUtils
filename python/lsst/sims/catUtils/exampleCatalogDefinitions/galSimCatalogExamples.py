25b7b9746fb27fcc16c9972452515f6e63ea2338
danielsf <scott.f.daniel@gmail.com>
2014-11-13 10:26:38 -0800

    galSimCatalogExamples.py now writes the SED corrected for redshift, dust, etc. to a scratch directory

--- python/lsst/sims/catUtils/exampleCatalogDefinitions/galSimCatalogExamples.py
+++ python/lsst/sims/catUtils/exampleCatalogDefinitions/galSimCatalogExamples.py
@@ -5,9 +5,10 @@ into an image.
 """
 
 import numpy
-from lsst.sims.catalogs.measures.instance import InstanceCatalog, cached
+import os
+from lsst.sims.catalogs.measures.instance import InstanceCatalog, cached, is_null
 from lsst.sims.coordUtils import CameraCoords, AstrometryGalaxies
-from lsst.sims.photUtils import EBVmixin
+from lsst.sims.photUtils import EBVmixin, Sed, CosmologyWrapper, Bandpass
 import lsst.afw.cameraGeom.testUtils as camTestUtils
 import lsst.afw.geom as afwGeom
 from lsst.afw.cameraGeom import PUPIL, PIXELS, FOCAL_PLANE
@@ -22,12 +23,11 @@ def radiansToArcsec(value):
 
 class GalSimBase(InstanceCatalog, CameraCoords):
 
-    cannot_be_null = ['sedFilepath']
+    cannot_be_null = ['galSimSedName']
 
-    column_outputs = ['galSimType', 'chipName', 'x_pupil', 'y_pupil', 'sedFilepath', 'magNorm',
+    column_outputs = ['galSimType', 'chipName', 'x_pupil', 'y_pupil', 'galSimSedName',
                       'majorAxis', 'minorAxis', 'sindex', 'halfLightRadius',
-                      'positionAngle', 'redshift',
-                      'internalAv', 'internalRv', 'galacticAv', 'galacticRv']
+                      'positionAngle']
 
     transformations = {'x_pupil':radiansToArcsec,
                        'y_pupil':radiansToArcsec}
@@ -36,8 +36,11 @@ class GalSimBase(InstanceCatalog, CameraCoords):
 
     delimiter = ';'
 
-    camera = camTestUtils.CameraWrapper().camera
+    objectCounter = 0
+    sedDirName = 'galSimSedDir'
 
+    camera = camTestUtils.CameraWrapper().camera
+    
     #if you want to use the actual LSST camera
     #camera = LsstSimMapper().camera
 
@@ -45,6 +48,17 @@ class GalSimBase(InstanceCatalog, CameraCoords):
         #copied from the phoSim catalogs
         return numpy.array([self.specFileMap[k] if self.specFileMap.has_key(k) else None
                          for k in self.column_by_name('sedFilename')])
+
+    def get_galSimSedName(self):
+        if os.path.exists(self.sedDirName):
+            if not os.path.isdir(self.sedDirName):
+                os.unlink(self.sedDirName)
+        if not os.path.exists(self.sedDirName):
+            os.mkdir(self.sedDirName)
+
+        returnNames = []
+        directory = os.path.join(os.getcwd(),self.sedDirName)
+        return self.calculateGalSimSed(directory)
 
     def write_header(self, file_handle):
         """
@@ -97,4 +111,62 @@ class GalSimGalaxies(GalSimBase, AstrometryGalaxies, EBVmixin):
     default_columns = [('galacticAv', 0.1, float),
                        ('galSimType', 'galaxy', (str,6))]
 
+    def calculateGalSimSed(self, outputDirectory):
+        actualSEDnames = self.column_by_name('sedFilepath')
+        redshift = self.column_by_name('redshift')
+        internalAv = self.column_by_name('internalAv')
+        internalRv = self.column_by_name('internalRv')
+        galacticAv = self.column_by_name('galacticAv')
+        galacticRv = self.column_by_name('galacticRv')
+        magNorm = self.column_by_name('magNorm')
+        
+        sedDir = os.getenv('SIMS_SED_LIBRARY_DIR')
 
+        #for setting magNorm
+        imsimband = Bandpass()
+        imsimband.imsimBandpass()
+        
+        cosmology = CosmologyWrapper()
+        
+        outputNames=[]
+        
+        for (sedName, zz, iAv, iRv, gAv, gRv, norm) in \
+            zip(actualSEDnames, redshift, internalAv, internalRv, galacticAv, galacticRv, magNorm):
+            
+            if is_null(sedName):
+                outputNames.append('None')
+            else:
+                #load the SED of the object
+                sedFile = os.path.join(sedDir, sedName)
+                sed = Sed()
+                sed.readSED_flambda(sedFile)
+
+                #normalize the SED
+                fNorm = sed.calcFluxNorm(norm, imsimband)
+                sed.multiplyFluxNorm(fNorm)
+
+                #apply dust extinction (internal)
+                a_int, b_int = sed.setupCCMab()
+                sed.addCCMDust(a_int, b_int, A_v=iAv, R_v=iRv)
+
+                #apply redshift; do not dim the SED; that should be left to the CosmologyWrapper
+                sed.redshiftSED(zz, dimming=False)
+
+                #apply cosmological distance modulus
+                luminosity = sed.calcFlux(imsimband)
+                distanceModulus = cosmology.distanceModulus(zz)
+                flux = luminosity * numpy.power(10.0, -0.4*distanceModulus)
+                sed.multiplyFluxNorm(flux)
+
+                #apply dust extinction (galactic)
+                a_int, b_int = sed.setupCCMab()
+                sed.addCCMDust(a_int, b_int, A_v=gAv, R_v=gRv)
+            
+                individualName = ('galSimSed_galaxy_%d.dat' % self.objectCounter)
+                name = os.path.join(outputDirectory,individualName)
+                outputNames.append(name)
+                self.objectCounter += 1
+                
+                sed.writeSED(name)
+
+        return numpy.array(outputNames)
