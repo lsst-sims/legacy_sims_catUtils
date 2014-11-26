321199628598cc5405c5a31e6968efbfbb01db78
danielsf <scott.f.daniel@gmail.com>
2014-11-26 09:51:33 -0800

    cleaned up whitespace

--- python/lsst/sims/catUtils/exampleCatalogDefinitions/refCatalogExamples.py
+++ python/lsst/sims/catUtils/exampleCatalogDefinitions/refCatalogExamples.py
@@ -12,16 +12,16 @@ class RefCatalogGalaxyBase(InstanceCatalog, AstrometryGalaxies, PhotometryGalaxi
     catalog_type = 'ref_catalog_galaxy'
     column_outputs = ['uniqueId', 'objId', 'galid', 'meanRaJ2000', 'meanDecJ2000', 'redshift',
                       'majorAxisDisk', 'minorAxisDisk', 'positionAngleDisk',
-                      'majorAxisBulge', 'minorAxisBulge', 'positionAngleBulge', 
+                      'majorAxisBulge', 'minorAxisBulge', 'positionAngleBulge',
                       'DiskLSSTu', 'DiskLSSTg', 'DiskLSSTr', 'DiskLSSTi', 'DiskLSSTz', 'DiskLSSTy',
                       'BulgeLSSTu', 'BulgeLSSTg', 'BulgeLSSTr', 'BulgeLSSTi', 'BulgeLSSTz', 'BulgeLSSTy',
-                      'u_ab', 'g_ab', 'r_ab', 'i_ab', 'z_ab', 'y_ab'] 
+                      'u_ab', 'g_ab', 'r_ab', 'i_ab', 'z_ab', 'y_ab']
     default_formats = {'S':'%s', 'f':'%.8f', 'i':'%i'}
     transformations = {'meanRaJ2000':numpy.degrees, 'meanDecJ2000':numpy.degrees, 'majorAxisDisk':numpy.degrees, 'minorAxisDisk':numpy.degrees,
-                       'positionAngleDisk':numpy.degrees, 'majorAxisBulge':numpy.degrees, 'minorAxisBulge':numpy.degrees, 
+                       'positionAngleDisk':numpy.degrees, 'majorAxisBulge':numpy.degrees, 'minorAxisBulge':numpy.degrees,
                        'positionAngleBulge':numpy.degrees}
 
-    def get_objectId(self): 
+    def get_objectId(self):
         return self.column_by_name(self.refIdCol)
 
     def get_meanRaJ2000(self):
@@ -34,23 +34,23 @@ class GalaxyPhotometry(RefCatalogGalaxyBase):
     catalog_type = 'galaxy_photometry_cat'
     column_outputs = ['uniqueId', 'objId', 'galid', 'meanRaJ2000', 'meanDecJ2000', 'redshift',
                       'majorAxisDisk', 'minorAxisDisk', 'positionAngleDisk',
-                      'majorAxisBulge', 'minorAxisBulge', 'positionAngleBulge', 
+                      'majorAxisBulge', 'minorAxisBulge', 'positionAngleBulge',
                       'internalAvBulge', 'internalAvDisk',
                       'uRecalc', 'gRecalc', 'rRecalc', 'iRecalc', 'zRecalc', 'yRecalc',
                       'uBulge', 'gBulge', 'rBulge', 'iBulge', 'zBulge', 'yBulge',
                       'uDisk', 'gDisk', 'rDisk', 'iDisk', 'zDisk', 'yDisk',
                       'uAgn', 'gAgn', 'rAgn', 'iAgn', 'zAgn', 'yAgn',
-                      'u_ab', 'g_ab', 'r_ab', 'i_ab', 'z_ab', 'y_ab'] 
+                      'u_ab', 'g_ab', 'r_ab', 'i_ab', 'z_ab', 'y_ab']
 
 class RefCatalogStarBase(InstanceCatalog, AstrometryGalaxies, PhotometryGalaxies):
     comment_char = ''
     catalog_type = 'ref_catalog_star'
-    column_outputs = ['uniqueId', 'objId', 'id', 'meanRaJ2000', 'meanDecJ2000', 
-                      'umag', 'gmag', 'rmag', 'imag', 'zmag', 'ymag'] 
+    column_outputs = ['uniqueId', 'objId', 'id', 'meanRaJ2000', 'meanDecJ2000',
+                      'umag', 'gmag', 'rmag', 'imag', 'zmag', 'ymag']
     default_formats = {'S':'%s', 'f':'%.8f', 'i':'%i'}
     transformations = {'meanRaJ2000':numpy.degrees, 'meanDecJ2000':numpy.degrees}
 
-    def get_objectId(self): 
+    def get_objectId(self):
         return self.column_by_name(self.refIdCol)
 
     def get_meanRaJ2000(self):
