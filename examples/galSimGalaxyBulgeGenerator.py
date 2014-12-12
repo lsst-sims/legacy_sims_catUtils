679bc98d6eddbcbf02f1808a877824840fb32d5b
danielsf <scott.f.daniel@gmail.com>
2014-12-11 16:16:09 -0800

    cleaned up galSimCatalogGenerator.py and renamed it galSimGalaxyBulgeGenerator.py

--- examples/galSimGalaxyBulgeGenerator.py
+++ examples/galSimGalaxyBulgeGenerator.py
@@ -1,21 +1,3 @@
-"""
-This file shows how to generate a GalSim input catalog named galSim_example.txt.
-galSimInterpreter.py will read that file in using the GalSimInterpreter class (see
-galSimInterpeter.py) and turn them into FITS images.
-
-This somewhat convoluted API (writing the catalog to a file and then reading it
-in with a separate script that uses GalSim to produce an image) is made necessary
-by the fact that we cannot install GalSim using the Anaconda version of Python on
-Macs
-
-stackoverflow.com/questions/23771608/trouble-installing-galsim-on-osx-with-anaconda
-
-To wit: if the user has a system python, SCons will try to install galsim using that
-python even if the user tells SCons to use the Anaconda python.  Therefore, we cannot
-use the stack-supplied python to run GalSim directly for the user.
-"""
-
-from __future__ import with_statement
 import os
 from lsst.sims.catalogs.measures.instance import InstanceCatalog
 from lsst.sims.catalogs.generation.db import CatalogDBObject, ObservationMetaData
@@ -44,5 +26,5 @@ galaxy_galSim = testGalSimGalaxies(gals, obs_metadata=obs_metadata)
 #galaxy_galSim.camera = LsstSimMapper().camera
 
 
-galaxy_galSim.write_catalog('galSim_example.txt', chunk_size=100)
-galaxy_galSim.write_images()
+galaxy_galSim.write_catalog('galSim_bulge_example.txt', chunk_size=100)
+galaxy_galSim.write_images(nameRoot='bulge')
