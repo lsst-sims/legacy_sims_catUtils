4031830d31c1eaa4b0920faa3885897e4852b408
danielsf <scott.f.daniel@gmail.com>
2014-12-12 15:56:20 -0800

    added tests for fits files containing multiple objects

--- tests/testGalSimInterface.py
+++ tests/testGalSimInterface.py
@@ -84,6 +84,7 @@ class GalSimInterfaceTest(unittest.TestCase):
         self.agnCatName = 'testGalaxyAgnCatalog.sav'
         self.starCatName = 'testStarCatalog.sav'
     
+    
     def tearDown(self):
         if os.path.exists(self.bulgeCatName):
             os.unlink(self.bulgeCatName)
@@ -102,44 +103,53 @@ class GalSimInterfaceTest(unittest.TestCase):
         del self.agnCatName
         del self.starCatName
     
+    
     def catalogTester(self, catName=None, catalog=None, nameRoot=None):
 
         catalog.write_catalog(catName)
         catalog.write_images(nameRoot=nameRoot)
         
-        drawnFilters = 0
+        galsimCounts = {}
+        controlCounts = {}
+        
+        for name in catalog.galSimInterpreter.detectorImages:
+            if nameRoot is not None:
+                name = nameRoot+'_'+name
+            im = afwImage.ImageF(name)
+            imArr = im.getArray()
+            galsimCounts[name[-6]] = imArr.sum()
+            controlCounts[name[-6]] = 0.0
+            #os.unlink(name)
+            
         with open(catName, 'r') as testFile:
             lines = testFile.readlines()
-            gg = lines[len(lines)-1].split(';')
-            sedName = gg[5]
-            magNorm = float(gg[11])
-            redshift = float(gg[12])
-            internalAv = float(gg[13])
-            internalRv = float(gg[14])
-            galacticAv = float(gg[15])
-            galacticRv = float(gg[16])
+            for line in lines:
+                if line[0] != '#':
+                    gg = line.split(';')
+                    sedName = gg[5]
+                    magNorm = float(gg[11])
+                    redshift = float(gg[12])
+                    internalAv = float(gg[13])
+                    internalRv = float(gg[14])
+                    galacticAv = float(gg[15])
+                    galacticRv = float(gg[16])
         
-            for name in catalog.galSimInterpreter.detectorImages:
-                if nameRoot is not None:
-                    name = nameRoot+'_'+name
-                im = afwImage.ImageF(name)
-                imArr = im.getArray()
-                galsimCounts = imArr.sum()
-                
-                filterName = name[-6]
-                bandPassName=os.path.join(eups.productDir('throughputs'),'baseline',('total_'+filterName+'.dat'))
-                bandpass = Bandpass()
-                bandpass.readThroughput(bandPassName)
-                controlCounts = calcADUwrapper(sedName=sedName, bandpass=bandpass, redshift=redshift, magNorm=magNorm,
-                                               internalAv=internalAv, internalRv=internalRv, galacticAv=galacticAv,
-                                               galacticRv=galacticRv)
-                
-                if controlCounts>1000.0:            
-                    self.assertTrue(numpy.abs(controlCounts-galsimCounts) < 0.05*galsimCounts)
+
+                    for filterName in controlCounts:
+                        bandPassName=os.path.join(eups.productDir('throughputs'),'baseline',('total_'+filterName+'.dat'))
+                        bandpass = Bandpass()
+                        bandpass.readThroughput(bandPassName)
+                        controlCounts[filterName] += calcADUwrapper(sedName=sedName, bandpass=bandpass,
+                                                                    redshift=redshift, magNorm=magNorm,
+                                                                    internalAv=internalAv, internalRv=internalRv, 
+                                                                    galacticAv=galacticAv, galacticRv=galacticRv)
+               
+            drawnFilters = 0
+            for ff in controlCounts:
+                if controlCounts[ff] > 1000.0:
                     drawnFilters += 1
-                
-                os.unlink(name)
-            
+                    self.assertTrue(numpy.abs(controlCounts[ff] - galsimCounts[ff]) < 0.05*controlCounts[ff])
+                    
             self.assertTrue(drawnFilters>4)
 
     def testGalaxyBulges(self):
@@ -172,7 +182,26 @@ class GalSimInterfaceTest(unittest.TestCase):
         gals = testGalaxyBulgeDBObj(address=self.connectionString)
         cat = psfCatalog(gals, obs_metadata = self.obs_metadata)
         self.catalogTester(catName=catName, catalog=cat, nameRoot='psf')
-    
+
+    def testMultipleImages(self):
+        dbName = 'galSimTestMultipleDB.db'
+        if os.path.exists(dbName):
+            os.unlink(dbName)
+        
+        displacedRA = numpy.array([72.0/3600.0, 50.0/3600.0, 75.0/3600.0])
+        displacedDec = numpy.array([0.0, 15.0/3600.0, -15.0/3600.0])
+        obs_metadata = makePhoSimTestDB(filename=dbName, size=1,
+                                            displacedRA=displacedRA, displacedDec=displacedDec)
+        connectionString = 'sqlite:///'+dbName
+        gals = testGalaxyBulgeDBObj(address=connectionString)
+        cat = testGalaxyCatalog(gals, obs_metadata=obs_metadata)
+        catName = 'multipleCatalog.sav'
+        self.catalogTester(catName=catName, catalog=cat, nameRoot='multiple')
+        
+        stars = testStarsDBObj(address=connectionString)
+        cat = testStarCatalog(stars, obs_metadata=obs_metadata)
+        catName = 'multipleStarCatalog.sav'
+        self.catalogTester(catName=catName, catalog=cat, nameRoot='multipleStars')
 
 def suite():
     utilsTests.init()
