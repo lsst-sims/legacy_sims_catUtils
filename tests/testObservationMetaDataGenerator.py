6edaa4a35b64584169bc460e4bd30af3de1fcc18
danielsf <scott.f.daniel@gmail.com>
2015-01-28 13:36:14 -0800

    added unit test for testObservationMetaDataGenerator.py

---
+++ tests/testObservationMetaDataGenerator.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+import lsst.utils.tests as utilsTests
+from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
+
+class ObservationMetaDataGeneratorTest(unittest.TestCase):
+
+    def testQuery(self):
+        myGen = ObservationMetaDataGenerator()
+        results = myGen.getObservationMetaData(fieldRA=(0.0, 0.1),
+                                               fieldDec=(-0.1, 0.1))
+
+        print results
+
+def suite():
+    utilsTests.init()
+    suites = []
+    suites += unittest.makeSuite(ObservationMetaDataGeneratorTest)
+
+    return unittest.TestSuite(suites)
+
+def run(shouldExit = False):
+    utilsTests.run(suite(), shouldExit)
+if __name__ == "__main__":
+    run(True)
