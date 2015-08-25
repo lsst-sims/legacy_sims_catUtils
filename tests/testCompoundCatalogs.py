c7d2039f75f84e9fc109b3903bbde791c719bdc6
danielsf <scott.f.daniel@gmail.com>
2015-08-25 14:39:47 -0700

    added unit test to make sure that CompoundInstanceCatalogs produce the same output as concatenated InstanceCatalogs.
    Only tests galaxies at this point (next few commits will fix that)

---
+++ tests/testCompoundCatalogs.py
@@ -0,0 +1,115 @@
+import unittest
+import os
+import lsst.utils.tests as utilsTests
+from lsst.utils import getPackageDir
+
+from lsst.sims.utils import ObservationMetaData
+from lsst.sims.catUtils.baseCatalogModels import GalaxyBulgeObj, GalaxyDiskObj, \
+                                                 GalaxyAgnObj, GalaxyTileCompoundObj
+
+from lsst.sims.catalogs.measures.instance import InstanceCatalog, CompoundInstanceCatalog
+
+class BulgeDiskCatalog(InstanceCatalog):
+    cannot_be_null = ['sedFilename']
+    column_outputs = ['galtileid', 'raJ2000', 'decJ2000',
+                     'componentra', 'componentdec',
+                     'magNorm', 'sedFilename',
+                     'majorAxis', 'minorAxis',
+                     'positionAngle',
+                     'halfLightRadius',
+                     'internalExtinctionModel',
+                     'internalAv', 'internalRv',
+                     ]
+
+
+class AgnCatalog(InstanceCatalog):
+    cannot_be_null = ['sedFilename']
+    column_outputs = ['galtileid', 'raJ2000', 'decJ2000',
+                      'componentra','componentdec',
+                      'magNorm', 'sedFilename',
+                      'variabilityParameters',
+                      ]
+
+class CompoundCatalogTest(unittest.TestCase):
+
+    def setUp(self):
+        self.baseDir = os.path.join(getPackageDir('sims_catUtils'), \
+                                    'tests', 'scratchSpace')
+
+    @unittest.expectedFailure
+    def testGalaxyCatalog(self):
+        """
+        Test GalaxyTileCompoundObj by creating a catalog of galaxy bulges, disks,
+        and agns using both the 'old fashioned way' (one catalog at a time), and
+        using CompoundInstanceCatalog
+        """
+        controlFileName = os.path.join(self.baseDir, 'gal_compound_control.txt')
+        testFileName = os.path.join(self.baseDir, 'gal_compound_test.txt')
+
+        if os.path.exists(controlFileName):
+            os.unlink(controlFileName)
+        if os.path.exists(testFileName):
+            os.unlink(testFileName)
+
+        obs = ObservationMetaData(unrefractedRA=25.0, unrefractedDec=-45.0,
+                                  boundType='circle', boundLength=0.05)
+
+        dbBulge = GalaxyBulgeObj()
+        dbDisk = GalaxyDiskObj()
+        dbAgn = GalaxyAgnObj()
+
+        catBulge = BulgeDiskCatalog(dbBulge, obs_metadata=obs)
+        catDisk = BulgeDiskCatalog(dbDisk, obs_metadata=obs)
+        catAgn = AgnCatalog(dbAgn, obs_metadata=obs)
+
+        catBulge.write_catalog(controlFileName, write_header=False, chunk_size=10000)
+        catDisk.write_catalog(controlFileName, write_mode='a', write_header=False, chunk_size=10000)
+        catAgn.write_catalog(controlFileName, write_mode='a', write_header=False, chunk_size=10000)
+
+
+        # You need to reinstantiate the catalogs because the process of writing them
+        # above stripped galtileid from their _active_columns, which is the only way
+        # CompoundInstanceCatalog can know that it needs to worry about galtileid
+        catBulge = BulgeDiskCatalog(dbBulge, obs_metadata=obs)
+        catDisk = BulgeDiskCatalog(dbDisk, obs_metadata=obs)
+        catAgn = AgnCatalog(dbAgn, obs_metadata=obs)
+
+        totalCat = CompoundInstanceCatalog([catBulge, catDisk, catAgn],
+                                           obs_metadata=obs,
+                                           compoundDBclass=GalaxyTileCompoundObj)
+
+        totalCat.write_catalog(testFileName, write_header=False, chunk_size=10000)
+
+        controlFile = open(controlFileName, 'r')
+        control = controlFile.readlines()
+        controlFile.close()
+
+        testFile = open(testFileName, 'r')
+        test = testFile.readlines()
+        testFile.close()
+
+        for line in control:
+            self.assertTrue(line in test)
+
+        for line in test:
+            self.assertTrue(line in control)
+
+        if os.path.exists(controlFileName):
+            os.unlink(controlFileName)
+
+        if os.path.exists(testFileName):
+            os.unlink(testFileName)
+
+
+
+def suite():
+    utilsTests.init()
+    suites = []
+    suites += unittest.makeSuite(CompoundCatalogTest)
+
+    return unittest.TestSuite(suites)
+
+def run(shouldExit = False):
+    utilsTests.run(suite(), shouldExit)
+if __name__ == "__main__":
+    run(True)
