76582d17821d58b5f098860cc695b77cf236b7ac
danielsf <scott.f.daniel@gmail.com>
2015-08-25 14:46:47 -0700

    added unit test to check combination of galaxies and stars

--- tests/testCompoundCatalogs.py
+++ tests/testCompoundCatalogs.py
@@ -5,7 +5,8 @@ from lsst.utils import getPackageDir
 
 from lsst.sims.utils import ObservationMetaData
 from lsst.sims.catUtils.baseCatalogModels import GalaxyBulgeObj, GalaxyDiskObj, \
-                                                 GalaxyAgnObj, GalaxyTileCompoundObj
+                                                 GalaxyAgnObj, GalaxyTileCompoundObj, \
+                                                 StarObj
 
 from lsst.sims.catalogs.measures.instance import InstanceCatalog, CompoundInstanceCatalog
 
@@ -29,6 +30,15 @@ class AgnCatalog(InstanceCatalog):
                       'magNorm', 'sedFilename',
                       'variabilityParameters',
                       ]
+
+
+class StarCatalog(InstanceCatalog):
+    cannot_be_null = ['sedFilename']
+    column_outputs = ['id', 'raJ2000', 'decJ2000',
+                      'glon', 'glat', 'magNorm',
+                      'properMotionRa', 'properMotionDec',
+                      'parallax', 'galacticAv', 'radialVelocity',
+                      'variabilityParameters', 'sedFilename']
 
 class CompoundCatalogTest(unittest.TestCase):
 
@@ -99,6 +109,74 @@ class CompoundCatalogTest(unittest.TestCase):
 
         if os.path.exists(testFileName):
             os.unlink(testFileName)
+
+    @unittest.expectedFailure
+    def testGalaxyAndStarCatalog(self):
+        """
+        Test GalaxyTileCompoundObj by creating a catalog of galaxy bulges, disks,
+        agns, and stars using both the 'old fashioned way' (one catalog at a time), and
+        using CompoundInstanceCatalog
+        """
+        controlFileName = os.path.join(self.baseDir, 'galStar_compound_control.txt')
+        testFileName = os.path.join(self.baseDir, 'galStar_compound_test.txt')
+
+        if os.path.exists(controlFileName):
+            os.unlink(controlFileName)
+        if os.path.exists(testFileName):
+            os.unlink(testFileName)
+
+        obs = ObservationMetaData(unrefractedRA=25.0, unrefractedDec=-45.0,
+                                  boundType='circle', boundLength=0.05)
+
+        dbBulge = GalaxyBulgeObj()
+        dbDisk = GalaxyDiskObj()
+        dbAgn = GalaxyAgnObj()
+        dbStar = StarObj()
+
+        catBulge = BulgeDiskCatalog(dbBulge, obs_metadata=obs)
+        catDisk = BulgeDiskCatalog(dbDisk, obs_metadata=obs)
+        catAgn = AgnCatalog(dbAgn, obs_metadata=obs)
+        catStar = StarCatalog(dbStar, obs_metadata=obs)
+
+        catBulge.write_catalog(controlFileName, write_header=False, chunk_size=10000)
+        catDisk.write_catalog(controlFileName, write_mode='a', write_header=False, chunk_size=10000)
+        catAgn.write_catalog(controlFileName, write_mode='a', write_header=False, chunk_size=10000)
+        catStar.write_catalog(controlFileName, write_mode='a', write_header=False, chunk_size=10000)
+
+
+        # You need to reinstantiate the catalogs because the process of writing them
+        # above stripped galtileid from their _active_columns, which is the only way
+        # CompoundInstanceCatalog can know that it needs to worry about galtileid
+        catBulge = BulgeDiskCatalog(dbBulge, obs_metadata=obs)
+        catDisk = BulgeDiskCatalog(dbDisk, obs_metadata=obs)
+        catAgn = AgnCatalog(dbAgn, obs_metadata=obs)
+        catStar = StarCatalog(dbStar, obs_metadata=obs)
+
+        totalCat = CompoundInstanceCatalog([catBulge, catDisk, catAgn, catStar],
+                                           obs_metadata=obs,
+                                           compoundDBclass=GalaxyTileCompoundObj)
+
+        totalCat.write_catalog(testFileName, write_header=False, chunk_size=10000)
+
+        controlFile = open(controlFileName, 'r')
+        control = controlFile.readlines()
+        controlFile.close()
+
+        testFile = open(testFileName, 'r')
+        test = testFile.readlines()
+        testFile.close()
+
+        for line in control:
+            self.assertTrue(line in test)
+
+        for line in test:
+            self.assertTrue(line in control)
+
+        #if os.path.exists(controlFileName):
+        #    os.unlink(controlFileName)
+
+        #if os.path.exists(testFileName):
+        #    os.unlink(testFileName)
 
 
 
