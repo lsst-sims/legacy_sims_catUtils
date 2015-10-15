ca419b51da21762daff173628fe727cb821cd0f9
danielsf <scott.f.daniel@gmail.com>
2015-10-15 09:59:34 -0700

    added unit test to verify that we can generate catalogs with multiple bandpass systems simultaneously

--- tests/testSSMmixins.py
+++ tests/testSSMmixins.py
@@ -5,7 +5,7 @@ import numpy as np
 import lsst.utils.tests as utilsTests
 from lsst.utils import getPackageDir
 from lsst.sims.catalogs.generation.db import fileDBObject
-from lsst.sims.catalogs.measures.instance import InstanceCatalog
+from lsst.sims.catalogs.measures.instance import InstanceCatalog, compound
 from lsst.sims.catUtils.mixins import PhotometrySSM
 from lsst.sims.photUtils import BandpassDict, SedList
 
@@ -13,6 +13,29 @@ class LSST_SSM_photCat(InstanceCatalog, PhotometrySSM):
     column_outputs = ['id', 'lsst_u' ,'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y']
 
     default_formats = {'f':'%.13f'}
+
+
+class LSST_SSM_multiplePhotCat(InstanceCatalog, PhotometrySSM):
+    column_outputs = ['id', 'lsst_u' ,'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y',
+                      'cartoon_u', 'cartoon_g', 'cartoon_r', 'cartoon_i', 'cartoon_z']
+
+    default_formats = {'f':'%.13f'}
+
+    @compound('cartoon_u', 'cartoon_g', 'cartoon_r', 'cartoon_i', 'cartoon_z')
+    def get_cartoon_mags(self):
+
+        if not hasattr(self, 'cartoonBandpassDict'):
+            bandpassDir = os.path.join(getPackageDir('sims_photUtils'), 'tests', 'cartoonSedTestData')
+
+            self.cartoonBandpassDict = BandpassDict.loadTotalBandpassesFromFiles(
+                                                      ['u', 'g', 'r', 'i', 'z'],
+                                                      bandpassDir = bandpassDir,
+                                                      bandpassRoot = 'test_bandpass_'
+                                                      )
+
+
+        return self._magnitudeGetter(self.cartoonBandpassDict, bandpassTag='cartoon')
+
 
 class SSMphotometryTest(unittest.TestCase):
 
@@ -55,6 +78,51 @@ class SSMphotometryTest(unittest.TestCase):
 
         if os.path.exists(catName):
             os.unlink(catName)
+
+
+    def testManyMagSystems(self):
+        """
+        Test that the SSM photometry mixin can simultaneously calculate magnitudes
+        in multiple bandpass systems
+        """
+        catName = os.path.join(getPackageDir('sims_catUtils'), 'tests', 'scratchSpace', 'compoundSsmPhotCat.txt')
+
+        cat=LSST_SSM_multiplePhotCat(self.photDB)
+        cat.write_catalog(catName)
+
+        dtype = np.dtype([('id', np.int), ('lsst_u', np.float), ('lsst_g', np.float),
+                          ('lsst_r', np.float), ('lsst_i', np.float), ('lsst_z', np.float),
+                          ('lsst_y', np.float),
+                          ('cartoon_u', np.float), ('cartoon_g', np.float),
+                          ('cartoon_r', np.float), ('cartoon_i', np.float),
+                          ('cartoon_z', np.float)])
+
+        testData = np.genfromtxt(catName, dtype=dtype, delimiter=',')
+
+        controlData = np.genfromtxt(self.dbFile, dtype=self.dtype)
+
+        LSSTbandpasses = BandpassDict.loadTotalBandpassesFromFiles()
+        cartoonBandpasses = BandpassDict.loadTotalBandpassesFromFiles(
+                                  ['u', 'g', 'r', 'i', 'z'],
+                                  bandpassDir = os.path.join(getPackageDir('sims_photUtils'), 'tests', 'cartoonSedTestData'),
+                                  bandpassRoot = 'test_bandpass_'
+                                  )
+
+        controlSedList = SedList(controlData['sedFilename'], controlData['magNorm'],
+                                 wavelenMatch=LSSTbandpasses.wavelenMatch)
+
+        controlLsstMags = LSSTbandpasses.magListForSedList(controlSedList)
+        controlCartoonMags = cartoonBandpasses.magListForSedList(controlSedList)
+
+        for ii in range(len(controlLsstMags)):
+            for jj, bpName in enumerate(['lsst_u', 'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y']):
+                self.assertAlmostEqual(controlLsstMags[ii][jj], testData[bpName][ii], 10)
+            for jj, bpName in enumerate(['cartoon_u', 'cartoon_g', 'cartoon_r', 'cartoon_i', 'cartoon_z']):
+                self.assertAlmostEqual(controlCartoonMags[ii][jj], testData[bpName][ii], 10)
+
+        if os.path.exists(catName):
+            os.unlink(catName)
+
 
 
 def suite():
