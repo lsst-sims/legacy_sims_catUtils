dc38e4145a468187a86a1f99d67a766523ffe68d
danielsf <scott.f.daniel@gmail.com>
2015-11-10 15:14:09 -0800

    add unit test to make sure that gamma is cached correctly in photometry mixins

--- tests/testArbitraryUncertaintyGetters.py
+++ tests/testArbitraryUncertaintyGetters.py
@@ -29,7 +29,7 @@ class PhotometryCartoon(object):
     @compound('sigma_cartoon_u', 'sigma_cartoon_g', 'sigma_cartoon_r', 'sigma_cartoon_i', 'sigma_cartoon_z')
     def get_cartoon_uncertainty(self):
         return self._magnitudeUncertaintyGetter(['cartoon_u', 'cartoon_g', 'cartoon_r', 'cartoon_i', 'cartoon_z'],
-                                                ['u', 'g', 'r', 'i', 'z', 'y'],
+                                                ['c_u', 'c_g', 'c_r', 'c_i', 'c_z', 'c_y'],
                                                  'cartoonBandpassDict')
 
 
@@ -55,7 +55,7 @@ class CartoonUncertaintyTestCase(unittest.TestCase):
         self.gband.readThroughput(os.path.join(getPackageDir('sims_photUtils'), 'tests', 'cartoonSedTestData', 'test_bandpass_g.dat'))
 
     def test_stars(self):
-        obs = ObservationMetaData(bandpassName=['u', 'g'],
+        obs = ObservationMetaData(bandpassName=['c_u', 'c_g'],
                                   m5=[25.0, 26.0])
         db_dtype = np.dtype([
                            ('id', np.int),
@@ -95,9 +95,80 @@ class CartoonUncertaintyTestCase(unittest.TestCase):
             self.assertAlmostEqual(umag, controlData['cartoon_u'][ix], 10)
             gmag = spectrum.calcMag(self.gband)
             self.assertAlmostEqual(gmag, controlData['cartoon_g'][ix], 10)
-            magError = calcMagError_m5(np.array([umag, gmag]), [self.uband, self.gband], [obs.m5['u'], obs.m5['g']], PhotometricParameters())
+            magError = calcMagError_m5(np.array([umag, gmag]), [self.uband, self.gband], [obs.m5['c_u'], obs.m5['c_g']], PhotometricParameters())
             self.assertAlmostEqual(magError[0], controlData['sigma_cartoon_u'][ix], 10)
             self.assertAlmostEqual(magError[1], controlData['sigma_cartoon_g'][ix], 10)
+
+
+    def test_mixed_stars(self):
+        """
+        Here we will test the (somewhat absurd) case of a catalog with two different bandpasses
+        (lsst_ and cartoon_) in order to verify that gamma values are being cached correctly
+        """
+
+        lsst_u_band = Bandpass()
+        lsst_u_band.readThroughput(os.path.join(getPackageDir('throughputs'), 'baseline', 'total_u.dat'))
+        lsst_g_band = Bandpass()
+        lsst_g_band.readThroughput(os.path.join(getPackageDir('throughputs'), 'baseline', 'total_g.dat'))
+
+        obs = ObservationMetaData(bandpassName=['c_u', 'c_g', 'u', 'g'],
+                                  m5=[25.0, 26.0, 15.0, 16.0])
+        # make the difference in m5 between the two bandpass systems extreme
+        # so that, in the unit test, we can be sure that the correct values
+        # are being used for the correct getters
+
+        db_dtype = np.dtype([
+                           ('id', np.int),
+                           ('raJ2000', np.float),
+                           ('decJ2000', np.float),
+                           ('sedFilename', str, 100),
+                           ('magNorm', np.float),
+                           ('galacticAv', np.float)
+                           ])
+
+        inputDir = os.path.join(getPackageDir('sims_catUtils'), 'tests', 'testData')
+        inputFile = os.path.join(inputDir, 'IndicesTestCatalogStars.txt')
+        db = fileDBObject(inputFile, dtype=db_dtype, runtable='test', idColKey='id')
+        catName = os.path.join(getPackageDir('sims_catUtils'), 'tests', 'scratchSpace', 'cartoonStarCat.txt')
+        cat = CartoonStars(db, obs_metadata=obs, column_outputs=['lsst_u', 'lsst_g', 'sigma_lsst_u', 'sigma_lsst_g'])
+        cat.write_catalog(catName)
+
+        dtype = np.dtype([(name, np.float) for name in cat._column_outputs])
+
+        controlData = np.genfromtxt(catName, dtype=dtype, delimiter=',')
+
+        if os.path.exists(catName):
+            os.unlink(catName)
+
+        db_columns = db.query_columns(['id', 'raJ2000', 'decJ2000', 'sedFilename', 'magNorm', 'galacticAv'])
+
+        sedDir = os.path.join(getPackageDir('sims_sed_library'), 'starSED', 'kurucz')
+
+        for ix, line in enumerate(db_columns.next()):
+            spectrum = Sed()
+            spectrum.readSED_flambda(os.path.join(sedDir, line[3]))
+            fnorm = spectrum.calcFluxNorm(line[4], self.normband)
+            spectrum.multiplyFluxNorm(fnorm)
+            a_x, b_x = spectrum.setupCCMab()
+            spectrum.addCCMDust(a_x, b_x, A_v=line[5])
+            umag = spectrum.calcMag(self.uband)
+            self.assertAlmostEqual(umag, controlData['cartoon_u'][ix], 10)
+            gmag = spectrum.calcMag(self.gband)
+            self.assertAlmostEqual(gmag, controlData['cartoon_g'][ix], 10)
+            lsst_umag = spectrum.calcMag(lsst_u_band)
+            self.assertAlmostEqual(lsst_umag, controlData['lsst_u'][ix], 10)
+            lsst_gmag = spectrum.calcMag(lsst_g_band)
+            self.assertAlmostEqual(lsst_gmag, controlData['lsst_g'][ix], 10)
+            magError = calcMagError_m5(np.array([umag, gmag]), [self.uband, self.gband], [obs.m5['c_u'], obs.m5['c_g']], PhotometricParameters())
+            self.assertAlmostEqual(magError[0], controlData['sigma_cartoon_u'][ix], 10)
+            self.assertAlmostEqual(magError[1], controlData['sigma_cartoon_g'][ix], 10)
+            lsst_magError = calcMagError_m5(np.array([lsst_umag, lsst_gmag]), [lsst_u_band, lsst_g_band],
+                                            [obs.m5['u'], obs.m5['g']], PhotometricParameters())
+
+            self.assertAlmostEqual(lsst_magError[0], controlData['sigma_lsst_u'][ix], 10)
+            self.assertAlmostEqual(lsst_magError[1], controlData['sigma_lsst_g'][ix], 10)
+            self.assertGreater(np.abs(lsst_magError[0]-magError[0]), 0.01)
+            self.assertGreater(np.abs(lsst_magError[1]-magError[1]), 0.01)
 
 
 def suite():
