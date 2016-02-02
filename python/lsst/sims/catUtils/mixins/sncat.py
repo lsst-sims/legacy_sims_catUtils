14a9dafa5a5928848dfcbf5b429cae2def7e54c3
R. Biswas <rbiswas4@gmail.com>
2016-02-02 10:40:19 -0800

    Adding code for phoSim images
    	modified:   ../python/lsst/sims/catUtils/mixins/sncat.py
    	modified:   ../python/lsst/sims/catUtils/supernovae/snObject.py

--- python/lsst/sims/catUtils/mixins/sncat.py
+++ python/lsst/sims/catUtils/mixins/sncat.py
@@ -31,6 +31,31 @@ class SNFunctionality(object):
     which define these attributes.
     """
 
+    def get_sedFilePath(self):
+        """
+        """
+        # construct the unique filename
+        # method: snid_mjd(to 4 places of decimal)_bandpassname
+        mjd = "_{:0.4f}_".format(self.mjdobs)
+        mjd += self.obs_metadata.bandpass + '.dat'
+        fname = ['specFile_' + str(elem) + mjd for elem in self.column_by_name('snid')]
+
+        c, x1, x0, t0  = self.column_by_name('c'),\
+                         self.column_by_name('x1'),\
+                         self.column_by_name('x0'),\
+                         self.column_by_name('t0')
+
+        SNobject = SNObject()
+        for i, in enumerate(self.column_by_name('snid')):
+            SNObject.set(c=c[i], x1=x1[i], x0=x0[i], t0=t0[i])
+
+            # SED in rest frame
+            sed = SNObject.SNObjectSourceSED
+
+
+        return fname
+
+
     def load_SNsed(self):
         """
         returns a list of SN seds in `lsst.sims.photUtils.Sed` observed within
