cf72bd7e866de4b3f8fdb8a597c830696603ec09
R. Biswas <rbiswas4@gmail.com>
2016-02-16 01:04:05 -0800

    rebased to master
    	modified:   EBVmixin.py

--- python/lsst/sims/catUtils/mixins/EBVmixin.py
+++ python/lsst/sims/catUtils/mixins/EBVmixin.py
@@ -14,10 +14,6 @@ class EBVmixin(EBVbase):
     This mixin class contains the getters which a catalog object will use to call
     calculateEbv in the EBVbase class
     """
-<<<<<<< HEAD
-=======
-
->>>>>>> calculate Av from Rv in InstanceCatalog
     #and finally, here is the getter
     @cached
     def get_EBV(self):
@@ -37,8 +33,5 @@ class EBVmixin(EBVbase):
         from getter (reading dustmaps) and the RV value from galacticRv
         """
         return self.column_by_name('EBV')*self.column_by_name('galacticRv')
-<<<<<<< HEAD
-=======
 
 
->>>>>>> calculate Av from Rv in InstanceCatalog
