2a33204807d812fec9eb2baa3005cc308b966043
danielsf <scott.f.daniel@gmail.com>
2016-06-01 17:38:49 -0700

    only search database once per field

--- python/lsst/sims/catUtils/utils/LightCurveGenerator.py
+++ python/lsst/sims/catUtils/utils/LightCurveGenerator.py
@@ -4,6 +4,7 @@ import numpy as np
 from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
 from  lsst.sims.catUtils.mixins import PhotometryStars, VariabilityStars
 from lsst.sims.catalogs.measures.instance import InstanceCatalog, compound
+from lsst.sims.utils import haversine
 
 import time
 
@@ -23,6 +24,21 @@ class _lightCurveCatalog(InstanceCatalog, VariabilityStars, PhotometryStars):
                  self.column_by_name("sigma_lsst_%s" % self.obs_metadata.bandpass)])
 
 
+class _chunkIterWrapper(object):
+
+    def __init__(self, data):
+        self._data = data
+        self._ix = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self._ix < len(self._data):
+            self._ix += 1
+            return self._data[self._ix-1]
+        else:
+            raise StopIteration
 
 
 class LightCurveGenerator(object):
@@ -50,16 +66,59 @@ class LightCurveGenerator(object):
         t_start = time.clock()
         print('starting light curve generation')
 
-        for ix, obs in enumerate(obs_list):
-            print('%d of %d at %e' % (ix, len(obs_list), time.clock()-t_start))
-            cat = _lightCurveCatalog(self._catalogdb, obs_metadata=obs)
-            for star_obj in cat.iter_catalog():
+        tol = 1.0e-12
+        obs_groups = []
+        for iobs, obs in enumerate(obs_list):
+            group_dex = -1
+
+            for ix, obs_g in enumerate(obs_groups):
+                dd = haversine(obs._pointingRA, obs._pointingDec,
+                               obs_list[obs_g[0]]._pointingRA, obs_list[obs_g[0]]._pointingDec)
+                if dd<tol:
+                    group_dex = ix
+                    break
+
+            if group_dex == -1:
+                obs_groups.append([iobs])
+            else:
+                obs_groups[group_dex].append(iobs)
+
+        for grp in obs_groups:
+            if len(grp) == 1:
+                obs = obs_list[grp[0]]
+                cat = _lightCurveCatalog(self._catalogdb, obs_metadata=obs)
+                for star_obj in cat.iter_catalog():
                     if star_obj[0] not in output_dict:
                         output_dict[star_obj[0]] = []
 
                     output_dict[star_obj[0]].append((obs.mjd.TAI,
                                                      star_obj[3],
                                                      star_obj[4]))
+
+            else:
+                dataCache = None
+                for ix in grp:
+                    obs = obs_list[ix]
+                    cat = _lightCurveCatalog(self._catalogdb, obs_metadata=obs)
+                    if dataCache is None:
+                        db_required_columns = cat.db_required_columns()
+                        query_result = cat.db_obj.query_columns(colnames=cat._active_columns,
+                                                               obs_metadata=obs,
+                                                               constraint=cat.constraint,
+                                                               chunk_size=100000)
+                        dataCache = []
+                        for chunk in query_result:
+                           dataCache.append(chunk)
+
+                    query_result = _chunkIterWrapper(dataCache)
+
+                    for star_obj in cat.iter_catalog(query_result=query_result):
+                        if star_obj[0] not in output_dict:
+                            output_dict[star_obj[0]] = []
+
+                        output_dict[star_obj[0]].append((obs.mjd.TAI,
+                                                         star_obj[3],
+                                                         star_obj[4]))
 
         print('that took %e' % (time.clock()-t_start))
         return output_dict
