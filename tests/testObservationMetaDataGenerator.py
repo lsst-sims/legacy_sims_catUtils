0519d7c2b8793b6307179eb4f2beef60a8ec35d8
danielsf <scott.f.daniel@gmail.com>
2016-07-06 11:56:35 -0700

    testObservationMetaData.py uses SearchReversion from utils/ directory

--- tests/testObservationMetaDataGenerator.py
+++ tests/testObservationMetaDataGenerator.py
@@ -8,60 +8,10 @@ from lsst.sims.utils import CircleBounds, BoxBounds
 from lsst.sims.catalogs.generation.db import CatalogDBObject
 from lsst.sims.catUtils.baseCatalogModels import GalaxyBulgeObj, GalaxyDiskObj, GalaxyAgnObj, StarObj
 from lsst.sims.catUtils.exampleCatalogDefinitions import PhoSimCatalogSersic2D
+from lsst.sims.catUtils.utils import SearchReversion, testGalaxyBulgeDBObj
 from lsst.sims.catalogs.generation.utils import makePhoSimTestDB
 from lsst.utils import getPackageDir
 
-#28 January 2015
-#SearchReversion and testGalaxyBulge are duplicated from testPhoSimCatalogs.py
-#There is already a pull request outstanding that will move
-#these classes to a testUtils.py file in sims_catUtils/../utils/
-#Once that pull request is merged, I will clean this file up
-#and remove the duplicated code.
-class SearchReversion(CatalogDBObject):
-    """
-    This is a mixin which is used below to force the galaxy CatalogDBObjects created for
-    this unittest to use the methods defined in the CatalogDBObject class.  This is because
-    we are using classes which inherit from GalaxyTileObj but do not actually want to use
-    the tiled query routines.
-
-    We also use this mixin for our stellar database object.  This is because StarObj
-    implements a query search based on htmid, which the test database for this unit
-    test will not have.
-    """
-
-    def _get_column_query(self, *args, **kwargs):
-        return CatalogDBObject._get_column_query(self,*args, **kwargs)
-
-    def _final_pass(self, *args, **kwargs):
-        return CatalogDBObject._final_pass(self,*args, **kwargs)
-
-    def query_columns(self, *args, **kwargs):
-        return CatalogDBObject.query_columns(self, *args, **kwargs)
-
-class testGalaxyBulge(SearchReversion, GalaxyBulgeObj):
-    """
-    A class for storing galaxy bulges
-    """
-    objid = 'phoSimTestBulges'
-    objectTypeId = 88
-
-    #The code below makes sure that we can store RA, Dec in degrees
-    #in the database but use radians in our calculations.
-    #We had to overwrite the original columns list because
-    #GalaxyTileObject daughter classes assume that RA and Dec are stored
-    #in radians in the database.  This is a side effect of the tiling
-    #scheme used to cover the whole sky.
-
-    columns = GalaxyBulgeObj.columns
-    _to_remove = []
-    for entry in columns:
-        if entry[0] == 'raJ2000' or entry[0] == 'decJ2000':
-            _to_remove.append(entry)
-    for target in _to_remove:
-        columns.remove(target)
-
-    columns.append(('raJ2000','ra*PI()/180.'))
-    columns.append(('decJ2000','dec*PI()/180.'))
 
 class ObservationMetaDataGeneratorTest(unittest.TestCase):
 
@@ -386,7 +336,7 @@ class ObservationMetaDataGeneratorTest(unittest.TestCase):
         if os.path.exists(dbName):
             os.unlink(dbName)
         _ = makePhoSimTestDB(filename=dbName)
-        bulgeDB = testGalaxyBulge(driver='sqlite', database=dbName)
+        bulgeDB = testGalaxyBulgeDBObj(driver='sqlite', database=dbName)
         gen = self.gen
         results = gen.getObservationMetaData(fieldRA=numpy.degrees(1.370916),
                                              telescopeFilter='i')
