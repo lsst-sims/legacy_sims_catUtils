1215697dd2f47536f87f91c623cf3a7c3dad85e9
danielsf <scott.f.daniel@gmail.com>
2016-08-17 10:07:47 -0700

    pep8 changes for testCosmologyMixins.py

--- tests/testCosmologyMixins.py
+++ tests/testCosmologyMixins.py
@@ -4,7 +4,6 @@ import lsst.utils.tests
 import numpy as np
 
 from lsst.sims.photUtils import CosmologyObject
-from lsst.sims.catalogs.definitions import InstanceCatalog
 
 from lsst.sims.catalogs.utils import myTestGals, makeGalTestDB
 
@@ -17,19 +16,19 @@ def setup_module(module):
 
 
 class cosmologicalGalaxyCatalog(testGalaxies, CosmologyMixin):
-    column_outputs = ['galid','lsst_u', 'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y',
+    column_outputs = ['galid', 'lsst_u', 'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y',
                       'uBulge', 'gBulge', 'rBulge', 'iBulge', 'zBulge', 'yBulge',
                       'uDisk', 'gDisk', 'rDisk', 'iDisk', 'zDisk', 'yDisk',
                       'uAgn', 'gAgn', 'rAgn', 'iAgn', 'zAgn', 'yAgn',
                       'redshift', 'cosmologicalDistanceModulus']
 
+
 class absoluteGalaxyCatalog(testGalaxies):
-    column_outputs = ['galid','lsst_u', 'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y',
+    column_outputs = ['galid', 'lsst_u', 'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y',
                       'uBulge', 'gBulge', 'rBulge', 'iBulge', 'zBulge', 'yBulge',
                       'uDisk', 'gDisk', 'rDisk', 'iDisk', 'zDisk', 'yDisk',
                       'uAgn', 'gAgn', 'rAgn', 'iAgn', 'zAgn', 'yAgn',
                       'redshift']
-
 
     def get_cosmologicalDistanceModulus(self):
         """
@@ -49,7 +48,7 @@ class CosmologyMixinUnitTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.dbName = 'cosmologyTestDB.db'
-        cls.dbSize=100
+        cls.dbSize = 100
         if os.path.exists(cls.dbName):
             os.unlink(cls.dbName)
         makeGalTestDB(size=cls.dbSize, seedVal=1, filename=cls.dbName)
@@ -97,7 +96,7 @@ class CosmologyMixinUnitTest(unittest.TestCase):
             self.assertEqual(cosmoRow[0], controlRow[0])
             self.assertEqual(cosmoRow[25], controlRow[25])
             self.assertEqual(cosmoRow[26], modulus)
-            for i in range(1,25):
+            for i in range(1, 25):
                 self.assertAlmostEqual(cosmoRow[i], controlRow[i] + modulus, 6)
 
 
