b1b09a091eba8bbad48805d8707a58311c17d395
danielsf <scott.f.daniel@gmail.com>
2017-04-12 16:50:45 -0700

    add comments to get_mdwarf_flares.sh

--- support_scripts/get_mdwarf_flares.sh
+++ support_scripts/get_mdwarf_flares.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# these variables indicate from where to download the
+# light curves
 data_url=https://lsst-web.ncsa.illinois.edu/
 data_dir=sim-data/mdwarf_flare_light_curves/
 data_file=mdwarf_flare_light_curves_170412.npz
 
 curl -O ${data_url}${data_dir}${data_file}
 
+# this is the directory where the light curve file
+# will be stored
 destination_dir=${SIMS_DATA_DIR}/catUtilsData
 
+# create destination_dir if it does not exist
 if [ ! -e $destination_dir ]
 then
     mkdir $destination_dir
