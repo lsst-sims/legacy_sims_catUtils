a708369f76c733736b98648c962552c80ba8965d
danielsf <scott.f.daniel@gmail.com>
2017-09-13 11:15:11 -0700

    add KeplerLightCurveMixin and method to load models

--- python/lsst/sims/catUtils/mixins/VariabilityMixin.py
+++ python/lsst/sims/catUtils/mixins/VariabilityMixin.py
@@ -66,6 +66,7 @@ import numpy as np
 import linecache
 import math
 import os
+import gzip
 import copy
 import numbers
 import json as json
@@ -80,7 +81,8 @@ from scipy.interpolate import interp1d
 __all__ = ["Variability", "VariabilityStars", "VariabilityGalaxies",
            "VariabilityAGN",
            "reset_agn_lc_cache", "StellarVariabilityModels",
-           "ExtraGalacticVariabilityModels", "MLTflaringMixin"]
+           "ExtraGalacticVariabilityModels", "MLTflaringMixin",
+           "KeplerLightCurveMixin"]
 
 _AGN_LC_CACHE = {}  # a global cache of agn light curve calculations
 
@@ -821,6 +823,53 @@ class MLTflaringMixin(Variability):
                                                  - local_base_mags)
 
         return dMags
+
+
+class KeplerLightCurveMixin(Variability):
+    """
+    This mixin models variability using parametrized functions fit to Kepler
+    light curves.
+    """
+
+    def _load_kepler_light_curves(self):
+        sims_data_dir = getPackageDir('sims_data')
+        lc_dir = os.path.join(sims_data_dir, 'catUtilsData')
+        lc_file = os.path.join(lc_dir, 'kplr_lc_params.txt.gz')
+        self._kepler_lc_models = {}
+        with gzip.open(lc_file, 'r') as input_file:
+            for line in input_file:
+                if line[0] == '#':
+                    continue
+                params = line.strip().split()
+                name = params[0]
+                tag = int(name.split('_')[0][4:])
+                n_c = int(params[3])
+                median = float(params[4+n_c])
+                local_aa = []
+                local_bb = []
+                local_cc = []
+                local_omega = []
+                local_tau = []
+
+                for i_c in range(n_c):
+                    base_dex = 5+n_c+i_c*5
+                    local_aa.append(float(params[base_dex]))
+                    local_bb.append(float(params[base_dex+1]))
+                    local_cc.append(float(params[base_dex+2]))
+                    local_omega.append(float(params[base_dex+3]))
+                    local_tau.append(float(params[base_dex+4]))
+                local_aa = np.array(local_aa)
+                local_bb = np.array(local_bb)
+                local_cc = np.array(local_cc)
+                local_omega = np.array(local_omega)
+                local_tau = np.array(local_tau)
+                self._kepler_lc_models[tag] = {}
+                self._kepler_lc_models[tag]['median'] = median
+                self._kepler_lc_models[tag]['a'] = local_aa
+                self._kepler_lc_models[tag]['b'] = local_bb
+                self._kepler_lc_models[tag]['c'] = local_cc
+                self._kepler_lc_models[tag]['omega'] = local_omega
+                self._kepler_lc_models[tag]['tau'] = local_tau
 
 
 class ExtraGalacticVariabilityModels(Variability):
