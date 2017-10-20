62d06781626d3ed3047b46139553259bb7d3bc6c
danielsf <scott.f.daniel@gmail.com>
2017-10-20 14:14:01 -0700

    put timing statements back in

--- python/lsst/sims/catUtils/mixins/VariabilityMixin.py
+++ python/lsst/sims/catUtils/mixins/VariabilityMixin.py
@@ -78,7 +78,7 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 from scipy.interpolate import UnivariateSpline
 from scipy.interpolate import interp1d
 
-# import time
+import time
 
 __all__ = ["Variability", "VariabilityStars", "VariabilityGalaxies",
            "VariabilityAGN", "StellarVariabilityModels",
@@ -741,7 +741,7 @@ class MLTflaringMixin(Variability):
         with the quiescent magnitudes of the objects
         """
 
-        # t_start = time.time()
+        t_start = time.time()
 
         if parallax is None:
             parallax = self.column_by_name('parallax')
@@ -863,7 +863,7 @@ class MLTflaringMixin(Variability):
         lc_name_arr = params['lc'].astype(str)
         lc_names_unique = np.sort(np.unique(lc_name_arr))
 
-        # t_work = 0.0
+        t_work = 0.0
 
         # load all of the necessary light curves
         # t_flux_dict = 0.0
@@ -910,11 +910,12 @@ class MLTflaringMixin(Variability):
             # t_flux_dict += time.time()-t_before_flux
 
 
-        # t_set_up = time.time()-t_start
 
         lc_dex_arr = np.ones(len(lc_name_arr), dtype=int)
         for i_lc in range(len(lc_name_arr)):
             lc_dex_arr[i_lc] = self._mlt_to_int[lc_name_arr[i_lc]]
+
+        t_set_up = time.time()-t_start
 
         dmag_master_dict = {}
 
@@ -944,21 +945,22 @@ class MLTflaringMixin(Variability):
 
                     flux_arr_dict[mag_name] = variability_cache['_MLT_LC_FLUX_CACHE']['%s_%s' % (lc_name, mag_name)]
 
-            # t_before_work = time.time()
-
+            t_before_work = time.time()
 
             self._process_mlt_class(lc_name_raw, lc_name_arr, lc_dex_arr, expmjd, params, time_arr, max_time, dt,
                                     flux_arr_dict, flux_factor, ebv, self._mlt_dust_lookup,
                                     base_fluxes, base_mags, mag_name_tuple, dmag_master_dict)
 
-            # t_work += time.time() - t_before_work
+            t_work += time.time() - t_before_work
 
         for lc_name in dmag_master_dict:
             for i_mag in dmag_master_dict[lc_name]['dmag']:
                 dMags[i_mag][dmag_master_dict[lc_name]['dex']] += dmag_master_dict[lc_name]['dmag'][i_mag]
 
-        # print('t MLT %.2e work %.2e setup %.2e flux_dict %.2e' %
-        #       (time.time()-t_start, t_work, t_set_up, t_flux_dict))
+        t_mlt = time.time()-t_start
+        print('t MLT %.2e work rat %.2e setup rat %.2e' %
+              (t_mlt, t_work/t_mlt, t_set_up/t_mlt))
+
         return dMags
 
 
