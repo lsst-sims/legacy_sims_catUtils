ea0c4846aba568965433538159eb7bb1582eb29e
danielsf <scott.f.daniel@gmail.com>
2017-11-01 15:09:19 -0700

    start trying to use avro in alertProcessor

--- python/lsst/sims/catUtils/utils/alertProcessor.py
+++ python/lsst/sims/catUtils/utils/alertProcessor.py
@@ -1,11 +1,19 @@
 # This script will provide classes to process the hdf5 files produced
 # by the AlertDataGenerator and write them as json objects
 
+try:
+    import avro.schema
+    from avro.io import DatumWriter
+    from avro.datafile import DataFileWriter
+except ImportError:
+    pass
+
 import h5py
 import os
 import numpy as np
 
 from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
+
 
 __all__ = ["AlertProcessor"]
 
@@ -15,9 +23,15 @@ class AlertProcessor(object):
     def __init__(self):
         self._open_file_handle = None
         self._open_file_name = None
+        self._diasource_schema = None
+        self._rng = np.random.RandomState(7123)
 
     def __del__(self):
         self._close_file()
+
+    def load_diasource_schema(self, file_name):
+        with open(file_name, "rb") as input_file:
+            self._diasource_schema = avro.schema.parse(input_file.read())
 
     def _close_file(self):
         if self._open_file_handle is None:
@@ -55,13 +69,110 @@ class AlertProcessor(object):
         self._open_file_handle = h5py.File(file_name, 'r')
         self._open_file_name = file_name
 
-    def _process_obs(self, obshistid, tai, bandpass, out_file_handle):
+    def _process_obs(self, obshistid, tai, bandpass, out_file_root):
         hdf_file_name = self._obshistid_to_file_name[obshistid]
         self._open_file(hdf_file_name)
+
+        bp_name_dict = {0: 'u', 1: 'g', 2: 'r', 3: 'i', 4: 'z', 5: 'y'}
+
+        with DataFileWriter(open("%s_%d.avro" % (out_file_root, obshistid), "wb"),
+                            DatumWriter(), self._diasource_schema) as data_writer:
+
+            for batch_ct in self._obshistid_ct_list[obshistid]:
+                field_root = '%d_%d' % (obshistid, batch_ct)
+                id_arr = self._open_file_handle['%s_uniqueId' % field_root].value
+                ra_arr = self._open_file_handle['%s_raICRS' % field_root].value
+                dec_arr = self._open_file_handle['%s_decICRS' % field_root].value
+                flux_arr = self._open_file_handle['%s_flux' % field_root].value
+                dflux_arr = self._open_file_handle['%s_dflux' % field_root].value
+                snr_arr = self._open_file_handle['%s_SNR' % field_root].value
+                chipnum_arr = self._open_file_handle['%s_chipNum' % field_root].value
+                xpix_arr = self._open_file_handle['%s_xPix' % field_root].value
+                ypix_arr = self._open_file_handle['%s_yPix' % field_root].value
+
+                for unqId, ra, dec, flux, dflux, snr, chipnum, xpix, ypix in \
+                zip(id_arr, ra_arr, dec_arr, flux_arr, dflux_arr, snr_arr, chipnum_arr,
+                    xpix_arr, ypix_arr):
+
+                    source = {}
+                    source['diaSourceId'] = unqId << 22 + obshistid
+                    source['ccdVisitId'] = chipnum*10**7 + obshistid
+                    source['diaObjectId'] = unqId
+                    source['midPointTai'] = tai
+                    source['filtername'] = bp_name_dict[bandpass]
+                    source['ra'] = ra
+                    source['decl'] = dec
+
+                    ra_dec_cov = {}
+                    ra_dec_cov['raSigma'] = self._rng.random_sample()*0.001
+                    ra_dec_cov['declSigma'] = self._rng.random_sample()*0.001
+                    ra_dec_cov['ra_decl_Cov'] = self._rng.random_sample()*0.001
+
+                    source['ra_decl_Cov'] = ra_dec_cov
+
+                    source['x'] = xpix
+                    source['y'] = ypix
+
+                    x_y_cov = {}
+                    x_y_cov['xSigma'] = self._rng.random_sample()*0.001*3600.0/0.2
+                    x_y_cov['ySigma'] = self._rng.random_sample()*0.001*3600.0/0.2
+                    x_y_cov['x_y_Cov'] = self._rng.random_sample()*0.001
+
+                    source['x_y_Cov'] = x_y_cov
+
+                    apFlux = {}
+                    apFlux['apMeanSb01'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb02'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb03'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb04'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb05'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb06'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb07'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb08'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb09'] = dflux * (1.0+self._rng.random_sample()-0.5)
+                    apFlux['apMeanSb10'] = dflux * (1.0+self._rng.random_sample()-0.5)
+
+                    source['apFlux'] = apFlux
+
+                    apFluxErr = {}
+                    apFluxErr['apMeanSb01Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb02Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb03Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb04Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb05Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb06Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb07Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb08Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb09Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+                    apFluxErr['apMeanSb10Err'] = dflux*(1.0+self._rng.random_sample()-0.5)/snr
+
+                    source['apFluxErr'] = apFluxErr
+
+                    source['snr'] = snr
+                    source['psFlux'] = dflux
+                    source['psRa'] = ra + (self._rng.random_sample()-0.5)*0.001
+                    source['psDecl'] = dec + (self._rng.random_sample()-0.5)*0.001
+
+                    ps_cov = {}
+                    ps_cov['psFluxSigma'] = dflux/snr
+                    ps_cov['psRaSigma'] = self._rng.random_sample()*0.001
+                    ps_cov['psDeclSigma'] = self._rng.random_sample()*0.001
+                    ps_cov['psFlux_psRa_Cov'] = self._rng.random_sample()*0.001
+                    ps_cov['psFlux_psDecl_Cov'] = self._rng.random_sample()*0.001
+                    ps_cov['psRa_psDecl_Cov'] = self._rng.random_sample()*0.001
+
+                    source['ps_Cov'] = ps_cov
+
+
+                    data_writer.append(source)
+
 
 
 
     def process(self, opsimdb, hdf5_dir, opsim_driver='sqlite'):
+
+        if self._diasource_schema is None:
+            raise RuntimeError("Need to specify diasource_schema")
 
         obs_gen = ObservationMetaDataGenerator(opsimdb, driver=opsim_driver)
         hdf5_list = []
