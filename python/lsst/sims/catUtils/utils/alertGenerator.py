1be1a3ab77b0e71872c1b6687d66165872600f45
danielsf <scott.f.daniel@gmail.com>
2017-12-02 22:50:49 -0800

    why does it freeze?

--- python/lsst/sims/catUtils/utils/alertGenerator.py
+++ python/lsst/sims/catUtils/utils/alertGenerator.py
@@ -940,6 +940,10 @@ class AlertDataGenerator(object):
                         if stdout_lock is not None:
                             stdout_lock.release()
 
+                if ct_dict['number_writing'] == 0:
+                    print('nothing writing %d is_least %s' % (this_pid, str(is_least)))
+                    print(ct_dict)
+
             if len(output_data_cache)>0:
                 if lock is not None:
                     lock.acquire()
