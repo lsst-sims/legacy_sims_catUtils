4be21649a7a6e37cda6e1b4ee95426e9cdcaeeff
danielsf <scott.f.daniel@gmail.com>
2017-12-08 11:18:25 -0800

    cache AGN in hdf5 files

--- workspace/htm_alert/cache_agn.py
+++ workspace/htm_alert/cache_agn.py
@@ -1,3 +1,4 @@
+import h5py
 import numpy as np
 import os
 import argparse
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--in_dir', type=str, default=None)
     parser.add_argument('--out_dir', type=str, default=None)
-    parser.add_argument('--t_step', type=float, default=365.25,
+    parser.add_argument('--t_step', type=int, default=365,
                         help="The length of time (in days) in which to "
                         "group light curves")
 
@@ -21,55 +22,42 @@ if __name__ == "__main__":
 
     print('cacheing AGN in pid %d' % os.getpid())
 
+    t_start_arr = np.arange(59580.0, 59580.0+3654.0, args.t_step)
+
     lc_dtype = np.dtype([('mjd', float), ('du', float), ('dg', float),
                          ('dr', float), ('di', float), ('dz', float)])
 
-    galid_list = []
-    normalizing_factors = {}
-    lc_data = {}
     list_of_all_files = os.listdir(args.in_dir)
     for file_name in list_of_all_files:
         if file_name.startswith('agn') and file_name.endswith('lc.txt'):
             full_name = os.path.join(args.in_dir, file_name)
             galid = int(file_name.split('_')[1])
-            galid_list.append(galid)
             with open(full_name, 'r') as in_file:
                 first_line = in_file.readline().strip()
                 params = first_line.split()
                 assert params[0] == '#'
-                normalizing_factors[galid] = []
+                normalizing_factors = []
                 for i_filter in range(6):
-                    normalizing_factors[galid].append(float(params[i_filter+1]))
+                    normalizing_factors.append(float(params[i_filter+1]))
 
-            raw_data = np.genfromtxt(full_name, dtype=lc_dtype)
-            lc_data[galid] = {}
-            lc_data[galid]['mjd'] = raw_data['mjd']
-            lc_data[galid]['du'] = raw_data['du']
+            lc_data = np.genfromtxt(full_name, dtype=lc_dtype)
 
-    t_start = 59580.0
-    duration = 3654.0
-    n_t_steps = np.floor(duration/args.t_step).astype(int)
-    print('read in all agn')
-    time.sleep(30)
-    exit(1)
+            duration = lc_data['mjd'][-1] - lc_data['mjd'][0]
+            dt = lc_data['mjd'][1]-lc_data['mjd'][0]
+            for i_t, t_start in enumerate(t_start_arr):
+                t_name = int(np.floor(t_start))
+                out_name = os.path.join(args.out_dir, 'agn_lcs_%d.hdf5' % t_name)
+                mjd_min = float(t_name)-5.0*dt
+                mjd_max = t_start+args.t_step+5.0*dt
+                if i_t == 0:
+                    valid = np.where(lc_data['mjd']<mjd_max)
+                elif i_t == (len(t_start_arr)-1):
+                    valid = np.where(lc_data['mjd']>mjd_min)
+                else:
+                    valid = np.where(np.logical_and(lc_data['mjd']>mjd_min,
+                                                    lc_data['mjd']<mjd_max))
 
-    for i_t, t_min in enumerate(np.arange(t_start, t_start+duration, args.t_step)):
-        t_max = t_min + args.t_step
-        output_lc = {}
-        print('time range %e %e' % (t_min, t_max))
-
-        for galid, file_name in zip(galid_list, lc_file_list):
-            data = np.genfromtxt(file_name, dtype=lc_dtype)
-            dt = data['mjd'][1] - data['mjd'][0]
-            valid = np.where(np.logical_and(data['mjd']>t_min-10.0*dt,
-                                            data['mjd']<t_max+10.0*dt))
-
-            mjd = data['mjd'][valid]
-            du = data['du'][valid]
-            output_lc['%d_mjd' % galid] = mjd
-            output_lc['%d_du' % galid] = du
-            output_lc['%d_norms' % galid] = normalizing_factors[galid]
-
-        out_file_name = os.path.join(args.out_dir, 'agn_lc_cache_%.1f_%.1f.npz' % (t_min, t_max))
-        with open(out_file_name, 'wb') as file_handle:
-            np.savez(file_handle, **output_lc)
+                with h5py.File(out_name, 'a') as out_file:
+                    out_file.create_dataset('%d_norm' % galid, data=normalizing_factors)
+                    out_file.create_dataset('%d_mjd' % galid, data=lc_data['mjd'][valid])
+                    out_file.create_dataset('%d_du' % galid, data=lc_data['du'][valid])
