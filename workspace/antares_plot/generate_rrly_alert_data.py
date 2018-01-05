673f1bfc0a5857fae310ec42824482d16eafb6a4
danielsf <scott.f.daniel@gmail.com>
2018-01-04 16:52:34 -0800

    make safe against n_rows==0

--- workspace/antares_plot/generate_rrly_alert_data.py
+++ workspace/antares_plot/generate_rrly_alert_data.py
@@ -41,17 +41,17 @@ def query_htmid(alert_gen, htmid_list, out_dir, out_prefix,
                                                  dmag_cutoff=dmag_cutoff,
                                                  photometry_class=AlertStellarVariabilityCatalog,
                                                  lock=lock,
-                                                 log_file_name=log_file_name,
-                                                 constraint='imag<24.0')
+                                                 log_file_name=log_file_name)
 
-        lock.acquire()
-        with open(log_file_name, 'a') as out_file:
-            elapsed = (time.time()-t_start)/3600.0
-            out_file.write('htmid %d nobs %d n_rows %d time %.2e hrs; per_row %.2e -- n_htmid %d of %d\n' %
-                           (htmid, alert_gen.n_obs(htmid), n_rows, elapsed,
-                            elapsed/n_rows, i_htmid, len(htmid_list)))
+        if n_rows>0:
+            lock.acquire()
+            with open(log_file_name, 'a') as out_file:
+                elapsed = (time.time()-t_start)/3600.0
+                out_file.write('htmid %d nobs %d n_rows %d time %.2e hrs; per_row %.2e -- n_htmid %d of %d\n' %
+                               (htmid, alert_gen.n_obs(htmid), n_rows, elapsed,
+                                elapsed/n_rows, i_htmid, len(htmid_list)))
 
-        lock.release()
+            lock.release()
 
 if __name__ == "__main__":
 
