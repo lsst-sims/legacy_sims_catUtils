aecd5a65cbd74cc5f46ddc8888b8ad26458d7e7f
danielsf <scott.f.daniel@gmail.com>
2018-01-10 11:29:22 -0800

    do test on length of light curve, first

--- workspace/antares_plot/extract_features.py
+++ workspace/antares_plot/extract_features.py
@@ -70,14 +70,18 @@ if __name__ == "__main__":
                     unq = unq_val[0]
                     g_time, g_flux, g_sig = get_lc(cursor, 1, unq)
                     i_time, i_flux, i_sig = get_lc(cursor, 3, unq)
+
+                    if len(g_flux)<3 or len(i_flux)<3:
+                        continue
+
                     dmag_g = 2.5*np.log10(1.0+(g_flux.max()-g_flux.min())/g_flux.min())
                     if dmag_g<0.005:
                         continue
+
                     dmag_i = 2.5*np.log10(1.0+(i_flux.max()-i_flux.min())/i_flux.min())
                     if dmag_i<0.005:
                         continue
-                    if len(g_flux)<3 or len(i_flux)<3:
-                        continue
+
                     g_entropy = FeatureExtraction.entropy(g_flux, g_sig)
                     i_entropy = FeatureExtraction.entropy(i_flux, i_sig)
                     g_hlr = FeatureExtraction.hlratio(g_flux)
