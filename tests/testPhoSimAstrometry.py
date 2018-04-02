cdc64aeb80c6a70888022a044c898f6cb8362a8c
danielsf <scott.f.daniel@gmail.com>
2018-04-02 09:55:33 -0700

    add test of deprecession based on focal plane coordinates

--- tests/testPhoSimAstrometry.py
+++ tests/testPhoSimAstrometry.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import numpy as np
 import tempfile
+import palpy
 
 import lsst.utils.tests
 from lsst.utils import getPackageDir
@@ -23,13 +24,89 @@ from lsst.sims.utils import _observedFromAppGeo
 from lsst.sims.utils import observedFromICRS
 from lsst.sims.utils import observedFromAppGeo
 
+from lsst.sims.utils import Site, ObservationMetaData
+from lsst.sims.utils import distanceToSun
+from lsst.sims.utils import raDecFromAltAz
+from lsst.sims.utils import pupilCoordsFromRaDec
+from lsst.sims.coordUtils import focalPlaneCoordsFromPupilCoords
+from lsst.sims.coordUtils import lsst_camera
+
 from lsst.sims.utils.CodeUtilities import sims_clean_up
+from lsst.sims.utils.CodeUtilities import _validate_inputs
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
 def setup_module(module):
     lsst.utils.tests.init()
+
+
+def _naivePupilCoordsFromObserved(ra_obs, dec_obs, ra0, dec0, rotSkyPos):
+    """
+    Convert Observed RA, Dec into pupil coordinates
+
+    Parameters
+    ----------
+    ra_obs is the observed RA in radians
+
+    dec_obs is the observed Dec in radians
+
+    obs_metadata is an ObservationMetaData characterizing the telescope location and pointing
+
+    epoch is the epoch of the mean RA and Dec in julian years (default=2000.0)
+
+    includeRefraction is a boolean controlling the application of refraction.
+
+    Returns
+    --------
+    A numpy array whose first row is the x coordinate on the pupil in
+    radians and whose second row is the y coordinate in radians
+    """
+
+    are_arrays = _validate_inputs([ra_obs, dec_obs], ['ra_obs', 'dec_obs'],
+                                  "pupilCoordsFromObserved")
+
+    theta = -1.0*rotSkyPos
+
+    ra_pointing = ra0
+    dec_pointing = dec0
+
+    # palpy.ds2tp performs the gnomonic projection on ra_in and dec_in
+    # with a tangent point at (pointingRA, pointingDec)
+    #
+    if not are_arrays:
+        try:
+            x, y = palpy.ds2tp(ra_obs, dec_obs, ra_pointing, dec_pointing)
+        except:
+            x = np.NaN
+            y = np.NaN
+    else:
+        try:
+            x, y = palpy.ds2tpVector(ra_obs, dec_obs, ra_pointing, dec_pointing)
+        except:
+            # apparently, one of your ra/dec values was improper; we will have to do this
+            # element-wise, putting NaN in the place of the bad values
+            x = []
+            y = []
+            for rr, dd in zip(ra_obs, dec_obs):
+                try:
+                    xx, yy = palpy.ds2tp(rr, dd, ra_pointing, dec_pointing)
+                except:
+                    xx = np.NaN
+                    yy = np.NaN
+                x.append(xx)
+                y.append(yy)
+            x = np.array(x)
+            y = np.array(y)
+
+    # rotate the result by rotskypos (rotskypos being "the angle of the sky relative to
+    # camera coordinates" according to phoSim documentation) to account for
+    # the rotation of the focal plane about the telescope pointing
+
+    x_out = x*np.cos(theta) - y*np.sin(theta)
+    y_out = x*np.sin(theta) + y*np.cos(theta)
+
+    return np.array([x_out, y_out])
 
 
 class StarTestCatalog(PhoSimAstrometryStars, InstanceCatalog):
@@ -67,6 +144,9 @@ class PhoSimAstrometryTestCase(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        if hasattr(lsst_camera, '_lsst_camera'):
+            del lsst_camera._lsst_camera
+
         sims_clean_up()
         if os.path.exists(cls.db_name):
             os.unlink(cls.db_name)
@@ -317,6 +397,77 @@ class PhoSimAstrometryTestCase(unittest.TestCase):
         self.assertGreater(dist_bad.min(), dist.max())
 
         del db
+
+    def test_naive_focal_plane_position(self):
+        """
+        Test deprecession of PhoSim coordinates by comparing
+        the focal plane position predicted by CatSim from ICRS
+        with the focal plane position predicted by CatSim from deprecessed
+        coordinates.
+        """
+
+        phosim_mixin = PhoSimAstrometryBase()
+
+        mjd = 59587.2
+
+        # create site with no atmosphere so that we can avoid
+        # refraction
+        site = Site(name="LSST", pressure=0.0, humidity=0.0)
+
+        obs = ObservationMetaData(mjd=mjd, site=site)
+        ra, dec = raDecFromAltAz(31.0, 112.0, obs)
+
+        d_sun = distanceToSun(ra, dec, obs.mjd)
+        self.assertGreater(d_sun, 45.0)
+
+        obs = ObservationMetaData(pointingRA=ra, pointingDec=dec,
+                                  rotSkyPos=27.3, mjd=mjd,
+                                  site=site)
+        ra_icrs = np.arange(obs.pointingRA-2.0, obs.pointingRA+2.0, 0.05)
+        dec_icrs = np.arange(obs.pointingDec-2.0, obs.pointingDec+2.0, 0.05)
+
+        coord_grid = np.meshgrid(ra_icrs, dec_icrs)
+        ra_icrs = coord_grid[0].flatten()
+        dec_icrs = coord_grid[1].flatten()
+
+        (xpup_icrs,
+         ypup_icrs) = pupilCoordsFromRaDec(ra_icrs, dec_icrs,
+                                           obs_metadata=obs,
+                                           epoch=2000.0,
+                                           includeRefraction=False)
+
+        (x_focal_icrs,
+         y_focal_icrs) = focalPlaneCoordsFromPupilCoords(xpup_icrs,
+                                                         ypup_icrs,
+                                                         camera=lsst_camera())
+
+        ra_obs, dec_obs = observedFromICRS(ra_icrs, dec_icrs, obs_metadata=obs,
+                                           epoch=2000.0,
+                                           includeRefraction=False)
+
+        ra_obs_rad = np.radians(ra_obs)
+        dec_obs_rad = np.radians(dec_obs)
+
+        (ra_deprecessed_rad,
+         dec_deprecessed_rad) = phosim_mixin._dePrecess(ra_obs_rad,
+                                                        dec_obs_rad, obs)
+
+        (xpup_deprecessed,
+         ypup_deprecessed) = _naivePupilCoordsFromObserved(ra_deprecessed_rad,
+                                                           dec_deprecessed_rad,
+                                                           obs._pointingRA,
+                                                           obs._pointingDec,
+                                                           obs._rotSkyPos)
+
+        (x_focal_deprecessed,
+         y_focal_deprecessed) = focalPlaneCoordsFromPupilCoords(xpup_deprecessed,
+                                                                ypup_deprecessed,
+                                                                camera=lsst_camera())
+
+        dd = np.sqrt((x_focal_icrs-x_focal_deprecessed)**2
+                     +(y_focal_icrs-y_focal_deprecessed)**2)
+
+        self.assertLess(dd.max(), 1.0e-8)
 
 
 class MemoryTestClass(lsst.utils.tests.MemoryTestCase):
