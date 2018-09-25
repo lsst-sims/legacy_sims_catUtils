1aed145bba11d6f4bc1f6f44126827e0b070e5b0
danielsf <scott.f.daniel@gmail.com>
2018-09-24 17:32:46 -0700

    print statement

--- python/lsst/sims/catUtils/mixins/VariabilityMixin.py
+++ python/lsst/sims/catUtils/mixins/VariabilityMixin.py
@@ -1370,6 +1370,7 @@ class ExtraGalacticVariabilityModels(Variability):
             current_batch = n_steps[0]
             for ii in range(1,len(n_steps),1):
                 if ii == len(n_steps)-1:
+                    print('batch ',current_batch,ii-i_start_arr[-1])
                     i_end_arr.append(len(n_steps))
                 else:
                     current_batch += n_steps[ii]
