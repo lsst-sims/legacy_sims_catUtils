0f62b3ae3a1c2d28112103d0811a366229b502d8
danielsf <scott.f.daniel@gmail.com>
2019-03-13 15:53:53 -0700

    fix some bugs

--- python/lsst/sims/catUtils/baseCatalogModels/LocalStarModels.py
+++ python/lsst/sims/catUtils/baseCatalogModels/LocalStarModels.py
@@ -30,7 +30,7 @@ class LocalStarCatalogConfig(pexConfig.Config):
     )
 
 
-class _HiddenStarCatalogObj(CatalogDBobject):
+class _HiddenStarCatalogObj(CatalogDBObject):
     config = LocalBaseCatalogConfig()
 
     #load $SIMS_CATUTILS_DIR/config/db.py
@@ -47,6 +47,20 @@ class _HiddenStarCatalogObj(CatalogDBobject):
     raColName = 'ra'
     decColName = 'decl'
     objectTypeId = 4
+
+    columns = [('id','simobjid', int),
+               ('raJ2000', 'ra*PI()/180.'),
+               ('decJ2000', 'decl*PI()/180.'),
+               ('glon', 'gal_l*PI()/180.'),
+               ('glat', 'gal_b*PI()/180.'),
+               ('magNorm', '(-2.5*log(flux_scale)/log(10.)) - 18.402732642'),
+               ('properMotionRa', '(mura/(1000.*3600.))*PI()/180.'),
+               ('properMotionDec', '(mudecl/(1000.*3600.))*PI()/180.'),
+               ('parallax', 'parallax*PI()/648000000.'),
+               ('galacticAv', 'CONVERT(float, ebv*3.1)'),
+               ('radialVelocity', 'vrad'),
+               ('variabilityParameters', 'varParamStr', str, 256),
+               ('sedFilename', 'sedfilename', str, 40)]
 
 
 class LocalStarChunkIterator(ChunkIterator):
