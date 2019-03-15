5dd7dc0435fdc86e066d7efe732f2faeb51fb785
danielsf <scott.f.daniel@gmail.com>
2019-03-15 10:59:56 -0700

    buid grid of test galaxies in testLocalGalaxyTileObj2.py

--- tests/testLocalGalaxyTileObj2.py
+++ tests/testLocalGalaxyTileObj2.py
@@ -1,12 +1,52 @@
 import unittest
 import numpy as np
-import time
+import sqlite3
+import shutil
+import tempfile
+import os
 
 import lsst.utils.tests
+from lsst.utils import getPackageDir
+
+import lsst.sims.utils.htmModule as htm
+from lsst.sims.catUtils.baseCatalogModels import LocalGalaxyModels as LocGal
 
 
 def setup_module(module):
     lsst.utils.tests.init()
+
+
+class GalaxyTileObjTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        dir_name = os.path.join(getPackageDir('sims_catUtils'), 'tests')
+        cls._tmpdir = tempfile.mkdtemp(dir=dir_name)
+
+        cls._temp_gal_db = os.path.join(cls._tmpdir, 'test_galaxies.db')
+
+        d_pos = 0.05
+        ra_grid = np.arange(-2.25, 2.251, d_pos)
+        dec_grid = np.arange(-2.25, 2.251, d_pos)
+        ra_dec_mesh = np.meshgrid(ra_grid, dec_grid)
+        ra_grid = ra_dec_mesh[0].flatten()
+        dec_grid = ra_dec_mesh[1].flatten()
+        galtag = 100*(45 + ra_grid/0.05) + (45+dec_grid/0.05)
+        assert len(galtag) == len(np.unique(galtag))
+
+        with sqlite3.connect(cls._temp_gal_db) as conn:
+            c = conn.cursor()
+            query = '''CREATE TABLE galaxy(htmid int, galid int,
+                       ra real, dec real, galtag int)'''
+            c.execute(query).fetchall()
+            values = ((r, d, g) for r, d, g in zip(ra_grid, dec_grid, gal_tag))
+            c.executemany('INSERT INTO galaxy(?,?,?)', values)
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.isdir(cls._tmpdir):
+            shutil.rmtree(cls._tmpdir)
+
 
 class MemoryTestClass(lsst.utils.tests.MemoryTestCase):
     pass
