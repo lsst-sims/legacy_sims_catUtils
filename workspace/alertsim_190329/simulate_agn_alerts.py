cbe4e1c21b34bc6774a586558fa63dfca0b026d2
danielsf <scott.f.daniel@gmail.com>
2019-04-11 15:03:19 -0700

    Revert "yet another diagnostic"
    This reverts commit e4519bd9a6e7466b8f3697816d24b6329bceef92.

--- workspace/alertsim_190329/simulate_agn_alerts.py
+++ workspace/alertsim_190329/simulate_agn_alerts.py
@@ -139,7 +139,6 @@ def process_agn_chunk(chunk, filter_obs, mjd_obs, m5_obs,
         flux_tot += agn_flux_tot
         mag_tot = dummy_sed.magFromFlux(flux_tot)
 
-        assert len(mag_dot) == n_t
         snr_single_val[:] = -1.0
         for i_bp, bp in enumerate('ugrizy'):
             valid = np.where(filter_obs==i_bp)
@@ -155,10 +154,7 @@ def process_agn_chunk(chunk, filter_obs, mjd_obs, m5_obs,
         noise_coadd = np.array([noise_coadd_cache[ii]
                                 for ii in filter_obs])
 
-        out_data[unq] = (mag_tot, agn_flux_tot,
-                         flux_gal[0][i_obj], flux_gal[1][i_obj],
-                         flux_gal[2][i_obj], flux_gal[3][i_obj],
-                         flux_gal[4][i_obj], flux_gal[5][i_obj])
+        out_data[unq] = (noise_single, flux_tot, snr_single_val)
         continue
 
         noise = np.sqrt(noise_coadd**2+noise_single**2)
