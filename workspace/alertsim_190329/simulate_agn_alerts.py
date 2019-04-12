855b36d411861f93f3e09e43602d727a07fc6029
danielsf <scott.f.daniel@gmail.com>
2019-04-12 09:47:29 -0700

    Revert "10k timesteps10k timesteps10k timesteps10k timesteps10k timesteps10k timesteps10k timesteps10k timesteps10k timesteps10k timesteps"
    This reverts commit 23856fa689511c015d474476645d373439658f06.

--- workspace/alertsim_190329/simulate_agn_alerts.py
+++ workspace/alertsim_190329/simulate_agn_alerts.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
 
     print('%d htmid' % len(htmid_to_obs))
 
-    threshold = 10000
+    threshold = 5000
     for kk in htmid_to_obs:
         n_obs = len(htmid_to_obs[kk])
         if n_obs>threshold and n_obs<2*threshold:
