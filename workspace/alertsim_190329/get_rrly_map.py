fb8b767fdc0adb86fde1db9ab6bf607764930e8f
danielsf <scott.f.daniel@gmail.com>
2019-04-18 14:06:08 -0700

    add map from lc_id to RRLyrae lc file name

---
+++ workspace/alertsim_190329/get_rrly_map.py
@@ -0,0 +1,14 @@
+import os
+rrly_dir = os.path.join(os.environ['SIMS_SED_LIBRARY_DIR'], 'rrly_lc')
+assert os.path.isdir(rrly_dir)
+
+with open('data/rrly_lc_map.txt', 'w') as out_file:
+    for sub_dir in ['RRab', 'RRc']:
+        full_dir = os.path.join(rrly_dir, sub_dir)
+        contents = os.listdir(full_dir)
+        for name in contents:
+            if not name.endswith('per.txt'):
+                continue
+            map_name = os.path.join(sub_dir, name)
+            lc_id = int(name.replace('_per.txt', ''))
+            out_file.write('%d;%s\n' % (lc_id, map_name))
