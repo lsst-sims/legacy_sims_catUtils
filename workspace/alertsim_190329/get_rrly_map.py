702923f26e01130e09de65708f4040d55fbd41fa
danielsf <scott.f.daniel@gmail.com>
2019-04-18 14:08:50 -0700

    make sure lc_id are not duplicated

--- workspace/alertsim_190329/get_rrly_map.py
+++ workspace/alertsim_190329/get_rrly_map.py
@@ -2,6 +2,7 @@ import os
 rrly_dir = os.path.join(os.environ['SIMS_SED_LIBRARY_DIR'], 'rrly_lc')
 assert os.path.isdir(rrly_dir)
 
+used_id = set()
 with open('data/rrly_lc_map.txt', 'w') as out_file:
     for sub_dir in ['RRab', 'RRc']:
         full_dir = os.path.join(rrly_dir, sub_dir)
@@ -11,4 +12,6 @@ with open('data/rrly_lc_map.txt', 'w') as out_file:
                 continue
             map_name = os.path.join(sub_dir, name)
             lc_id = int(name.replace('_per.txt', ''))
+            assert lc_id not in used_id
+            used_id.add(lc_id)
             out_file.write('%d;%s\n' % (lc_id, map_name))
