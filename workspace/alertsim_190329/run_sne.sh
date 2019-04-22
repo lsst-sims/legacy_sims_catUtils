53e0dd739ad6f65be8c59f2de48e7d8647af5e5e
danielsf <scott.f.daniel@gmail.com>
2019-04-22 15:38:28 -0700

    add scripts to run stars and sne

---
+++ workspace/alertsim_190329/run_sne.sh
@@ -0,0 +1,25 @@
+out_dir=/astro/store/pogo4/danielsf/alertsim_190329/
+
+python -W'ignore' simulate_sne_alerts.py \
+--circular_fov --htmid_list data/region_4_trixels.txt \
+--out_name=${out_dir}region_4_sne.pickle
+
+echo "completed sne region 4"
+
+python -W'ignore' simulate_sne_alerts.py \
+--circular_fov --htmid_list data/region_1_trixels.txt \
+--out_name=${out_dir}region_1_sne.pickle
+
+echo "completed sne region 1"
+
+python -W'ignore' simulate_sne_alerts.py \
+--circular_fov --htmid_list data/region_2_trixels.txt \
+--out_name=${out_dir}region_2_sne.pickle
+
+echo "completed sne region 2"
+
+python -W'ignore' simulate_sne_alerts.py \
+--circular_fov --htmid_list data/region_3_trixels.txt \
+--out_name=${out_dir}region_3_sne.pickle
+
+echo "completed sne region 3"
