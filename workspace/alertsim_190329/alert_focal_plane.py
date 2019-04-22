dca78f8e5114cc4112e9e7a1c46fa0c1470fed6e
danielsf <scott.f.daniel@gmail.com>
2019-04-22 15:09:45 -0700

    add random 90 pct filter to improper focal plane

--- workspace/alertsim_190329/alert_focal_plane.py
+++ workspace/alertsim_190329/alert_focal_plane.py
@@ -9,6 +9,11 @@ def apply_focal_plane(ra, dec, photometry_mask_1d,
     n_obj = len(ra)
     n_t = len(filter_obs)
 
+    if not proper_chip:
+        seed = int(np.round(ra[0]+dec[0]+obs_md_list[0].mjd.TAI))
+        rng = np.random.RandomState(seed)
+        focal_plane_roll = rng.random_sample(len(ra))
+
     chip_mask = np.zeros((n_obj, n_t), dtype=bool)
     for i_t, (obs, i_bp) in enumerate(zip(obs_md_list, filter_obs)):
         if proper_chip:
@@ -23,7 +28,7 @@ def apply_focal_plane(ra, dec, photometry_mask_1d,
                                    dec[photometry_mask_1d],
                                    obs.pointingRA, obs.pointingDec)
 
-            valid_chip = (dd<=fov_radius)
+            valid_chip = (dd<=fov_radius)&(focal_plane_roll<0.9)
 
         chip_mask[photometry_mask_1d, i_t] = valid_chip
 
