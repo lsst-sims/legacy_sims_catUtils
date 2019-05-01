f85d3cf0f1495ae3770d04bd36f5ca87f0e4cfa4
danielsf <scott.f.daniel@gmail.com>
2019-05-01 15:38:47 -0700

    assess validity before acquiring lock

--- workspace/alertsim_190329/simulate_stellar_alerts.py
+++ workspace/alertsim_190329/simulate_stellar_alerts.py
@@ -267,13 +267,17 @@ def process_stellar_chunk(chunk, filter_obs, mjd_obs, m5_obs,
                 var_type_out[i_obj] = chunk['var_type'][i_obj]
 
     valid = np.where(unq_out>=0)
+    unq_out = unq_out[valid]
+    mjd_out = mjd_out[valid]
+    var_type_out = var_type_out[valid]
+    snr_out = snr_out[valid]
     with lock:
         existing_keys = list(out_data.keys())
         key_val = 0
         while key_val in existing_keys:
             key_val += 1
-        out_data[key_val] = (unq_out[valid], mjd_out[valid],
-                             snr_out[valid], var_type_out[valid])
+        out_data[key_val] = (unq_out, mjd_out,
+                             snr_out, var_type_out)
 
     #print('%d tot %d first %d at all %d ' %
     #(os.getpid(),ct_tot, ct_first, ct_at_all))
